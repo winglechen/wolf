48caa6ebed7c1369743423ba20fcd202c34c0d72
思源 <winglechen@gmail.com>
2019-09-27 17:32:41 +0800

    add licenses for account

---
+++ business/account/account-api/src/main/java/study/daydayup/wolf/business/account/api/entity/license/JwtLicense.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.account.api.entity.license;
+
+/**
+ * study.daydayup.wolf.business.account.api.entity.license
+ *
+ * @author Wingle
+ * @since 2019/9/27 5:28 PM
+ **/
+public class JwtLicense extends License {
+}
