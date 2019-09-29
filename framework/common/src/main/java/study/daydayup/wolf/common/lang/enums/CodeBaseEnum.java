403d61da4b264079927c05cf992444d7e4ea9464
思源 <winglechen@gmail.com>
2019-09-29 23:01:08 +0800

    add CodeBasedEnum interface

---
+++ framework/common/src/main/java/study/daydayup/wolf/common/lang/enums/CodeBaseEnum.java
@@ -0,0 +1,11 @@
+package study.daydayup.wolf.common.lang.enums;
+
+/**
+ * study.daydayup.wolf.common.lang.enums
+ *
+ * @author Wingle
+ * @since 2019/9/29 10:21 PM
+ **/
+public interface CodeBaseEnum {
+    int getCode();
+}
---
+++ framework/common/src/main/java/study/daydayup/wolf/common/util/EnumUtil.java
@@ -0,0 +1,14 @@
+package study.daydayup.wolf.common.util;
+
+import study.daydayup.wolf.common.lang.enums.CodeBaseEnum;
+import study.daydayup.wolf.common.lang.enums.GenderEnum;
+
+/**
+ * study.daydayup.wolf.common.util
+ *
+ * @author Wingle
+ * @since 2019/9/29 10:22 PM
+ **/
+public class EnumUtil {
+
+}
