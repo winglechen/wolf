3c177547e297df73cdc1ace12560819d8b7c2ec2
思源 <winglechen@gmail.com>
2019-09-29 16:34:41 +0800

    add biz and deploy for account

---
+++ business/account/account-deploy-web/src/main/java/study/daydayup/wolf/business/account/deploy/web/AccountApplication.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.account.deploy.web;
+
+/**
+ * study.daydayup.wolf.business.account.deploy.web
+ *
+ * @author Wingle
+ * @since 2019/9/29 2:13 PM
+ **/
+public class AccountApplication {
+}
