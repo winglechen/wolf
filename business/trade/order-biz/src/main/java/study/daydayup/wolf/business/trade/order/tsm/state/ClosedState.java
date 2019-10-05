69c2e54f204de7746875511690540f0a83e005e0
思源 <winglechen@gmail.com>
2019-10-05 23:26:29 +0800

    add empty TradeStates

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/tsm/state/ClosedState.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.order.tsm.state;
+
+/**
+ * study.daydayup.wolf.business.trade.order.tsm.state
+ *
+ * @author Wingle
+ * @since 2019/10/5 11:23 PM
+ **/
+public class ClosedState implements TradeState {
+}
