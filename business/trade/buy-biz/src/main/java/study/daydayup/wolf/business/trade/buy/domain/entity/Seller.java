a94cc0da405e98ed78c677da078e9df0814152d7
思源 <winglechen@gmail.com>
2019-10-05 13:58:26 +0800

    add domain objects to  trade.buy

---
+++ business/trade/buy-biz/src/main/java/study/daydayup/wolf/business/trade/buy/domain/entity/Seller.java
@@ -0,0 +1,15 @@
+package study.daydayup.wolf.business.trade.buy.domain.entity;
+
+import study.daydayup.wolf.framework.layer.api.Model;
+
+/**
+ * study.daydayup.wolf.business.trade.buy.domain.entity
+ *
+ * @author Wingle
+ * @since 2019/10/5 1:40 PM
+ **/
+public class Seller extends Model {
+    private long sellerId;
+    private String sellerName;
+    private String sellerLogo;
+}
---
+++ business/trade/buy-biz/src/main/java/study/daydayup/wolf/business/trade/buy/domain/factory/TradeFlowFactory.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.buy.domain.factory;
+
+/**
+ * study.daydayup.wolf.business.trade.buy.domain.factory
+ *
+ * @author Wingle
+ * @since 2019/10/5 11:32 AM
+ **/
+public class TradeFlowFactory {
+}
