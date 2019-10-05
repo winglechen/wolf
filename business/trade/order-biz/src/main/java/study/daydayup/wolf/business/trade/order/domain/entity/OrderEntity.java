d805bc0ea6f2a827dedb51dc904c96bca26aa7f9
思源 <winglechen@gmail.com>
2019-10-05 19:23:35 +0800

    add orderEntity and tsm package

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/domain/entity/OrderEntity.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.business.trade.order.domain.entity;
+
+import study.daydayup.wolf.business.trade.api.model.Order;
+
+/**
+ * study.daydayup.wolf.business.trade.order.domain.entity
+ *
+ * @author Wingle
+ * @since 2019/10/5 7:21 PM
+ **/
+public class OrderEntity extends Order {
+}
---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/tsm/README.md
@@ -0,0 +1,1 @@
+trade state machine
