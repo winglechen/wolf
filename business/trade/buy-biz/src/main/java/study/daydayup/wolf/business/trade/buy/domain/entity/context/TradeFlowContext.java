b8b6cea0132d3ad7ed742415cd0e2a42387a4188
思源 <winglechen@gmail.com>
2019-10-05 13:56:26 +0800

    modify TradeFlowContext

--- business/trade/buy-biz/src/main/java/study/daydayup/wolf/business/trade/buy/domain/entity/context/TradeFlowContext.java
+++ business/trade/buy-biz/src/main/java/study/daydayup/wolf/business/trade/buy/domain/entity/context/TradeFlowContext.java
@@ -1,5 +1,9 @@
 package study.daydayup.wolf.business.trade.buy.domain.entity.context;
 
+import study.daydayup.wolf.business.trade.api.model.buy.Buyer;
+import study.daydayup.wolf.business.trade.buy.domain.entity.Seller;
+import study.daydayup.wolf.business.trade.buy.domain.entity.TradeGoods;
+
 /**
  * study.daydayup.wolf.business.trade.buy.domain.entity
  *
@@ -7,4 +11,7 @@ package study.daydayup.wolf.business.trade.buy.domain.entity.context;
  * @since 2019/10/5 11:01 AM
  **/
 public class TradeFlowContext {
+    private Buyer buyer;
+    private Seller seller;
+    private TradeGoods[] goods;
 }
