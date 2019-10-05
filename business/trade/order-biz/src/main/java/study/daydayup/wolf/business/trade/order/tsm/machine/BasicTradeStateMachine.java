102dd872f85fc3709b2c08859ab5e4d03c7fbe1a
思源 <winglechen@gmail.com>
2019-10-05 23:22:21 +0800

    add empty tradeStateMachines

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/tsm/machine/BasicTradeStateMachine.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.order.tsm.machine;
+
+/**
+ * study.daydayup.wolf.business.trade.order.tsm.machine
+ *
+ * @author Wingle
+ * @since 2019/10/5 11:20 PM
+ **/
+public class BasicTradeStateMachine extends AbstractTradeStateMachine implements TradeStateMachine {
+}
