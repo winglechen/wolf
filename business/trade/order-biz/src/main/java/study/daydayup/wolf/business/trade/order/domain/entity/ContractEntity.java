d717bb4955527b3c07b6df920f39b26af42de9bf
思源 <winglechen@gmail.com>
2019-10-07 23:55:11 +0800

    add empty contract

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/domain/entity/ContractEntity.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.order.domain.entity;
+
+/**
+ * study.daydayup.wolf.business.trade.order.domain.entity
+ *
+ * @author Wingle
+ * @since 2019/10/7 11:54 下午
+ **/
+public class ContractEntity {
+}
