a695c8ebe4d2e993b24bbdbc1d9c4bcfad08a074
思源 <winglechen@gmail.com>
2019-10-09 22:02:05 +0800

    add order.dal.po package

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/dal/po/OrderLinePO.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.order.dal.po;
+
+/**
+ * study.daydayup.wolf.business.trade.order.dal.po
+ *
+ * @author Wingle
+ * @since 2019/10/9 9:47 下午
+ **/
+public class OrderLinePO {
+}
