4ada328058de03bb2aa43af13d0dad781665fae9
思源 <winglechen@gmail.com>
2019-10-09 22:08:14 +0800

    add trade.order.dal.cao package

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/dal/cao/ContractDAO.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.order.dal.cao;
+
+/**
+ * study.daydayup.wolf.business.trade.order.dal.cao
+ *
+ * @author Wingle
+ * @since 2019/10/9 10:06 下午
+ **/
+public interface ContractDAO {
+}
---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/dal/cao/OrderDetailDAO.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.order.dal.cao;
+
+/**
+ * study.daydayup.wolf.business.trade.order.dal.cao
+ *
+ * @author Wingle
+ * @since 2019/10/9 10:03 下午
+ **/
+public interface OrderDetailDAO {
+}
