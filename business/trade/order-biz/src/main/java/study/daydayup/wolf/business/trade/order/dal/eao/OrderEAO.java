3b4efd40293aa892cc42e90c343f1c1b08961e0b
思源 <winglechen@gmail.com>
2019-10-09 22:13:54 +0800

    add trade.order cao、eao、hao package

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/dal/eao/OrderEAO.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.order.dal.eao;
+
+/**
+ * study.daydayup.wolf.business.trade.order.dal.eao
+ *
+ * @author Wingle
+ * @since 2019/10/9 10:11 下午
+ **/
+public class OrderEAO {
+}
