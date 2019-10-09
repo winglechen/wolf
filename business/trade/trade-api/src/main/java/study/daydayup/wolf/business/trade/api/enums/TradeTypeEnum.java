599defe113959a375256a4667470c06e68d99f3f
思源 <winglechen@gmail.com>
2019-10-09 14:23:03 +0800

    add blank line for TradeTypeEnum

--- business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/enums/TradeTypeEnum.java
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/enums/TradeTypeEnum.java
@@ -13,10 +13,13 @@ import study.daydayup.wolf.common.lang.enums.CodeBasedEnum;
 public enum TradeTypeEnum implements CodeBasedEnum {
     GIFT_CREATE(20, "送礼"),
     GIFT_RECEIVE(21, "收礼"),
+
     PEERPAY_CREATE(40, "代付"),
     PEERPAY_PAY(41, "代付支付"),
+
     GROUP_BUY(50, "团购"),
     GROUP_PARTICPATE(51, "团购参与"),
+
     FENXIAO(60, "分销"),
     NORMAL(10, "普通")
     ;
