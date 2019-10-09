a955e6d885137d92cfa907be789932c7efc74f7c
思源 <winglechen@gmail.com>
2019-10-09 11:42:52 +0800

    add TradeState model and TradeNo vo

---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/model/TradeState.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.business.trade.api.model;
+
+import study.daydayup.wolf.framework.layer.api.Model;
+
+/**
+ * study.daydayup.wolf.business.trade.api.model.buy
+ *
+ * @author Wingle
+ * @since 2019/10/9 11:40 上午
+ **/
+public class TradeState extends Model {
+}
---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/vo/TradeNo.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.api.vo;
+
+/**
+ * study.daydayup.wolf.business.trade.api.vo
+ *
+ * @author Wingle
+ * @since 2019/10/9 11:39 上午
+ **/
+public class TradeNo {
+}
