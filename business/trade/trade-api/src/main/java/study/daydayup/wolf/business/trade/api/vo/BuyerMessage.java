3dd31ee97fc18e3b650d7695f01d7e673e2cb47e
思源 <winglechen@gmail.com>
2019-10-10 12:06:06 +0800

    add buyerMessage and sellerMemo

---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/vo/BuyerMessage.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.api.vo;
+
+/**
+ * study.daydayup.wolf.business.trade.api.vo
+ *
+ * @author Wingle
+ * @since 2019/10/10 12:01 下午
+ **/
+public class BuyerMessage {
+}
---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/vo/SellerMemo.java
@@ -0,0 +1,15 @@
+package study.daydayup.wolf.business.trade.api.vo;
+
+import lombok.Data;
+
+/**
+ * study.daydayup.wolf.business.trade.api.vo
+ *
+ * @author Wingle
+ * @since 2019/10/10 12:03 下午
+ **/
+@Data
+public class SellerMemo {
+    private int star;
+    private String memo;
+}
