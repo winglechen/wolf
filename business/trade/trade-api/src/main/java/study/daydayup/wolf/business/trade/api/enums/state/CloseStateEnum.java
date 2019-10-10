e0b005fd0d76600c3bd52cf66193068a29875a1b
思源 <winglechen@gmail.com>
2019-10-10 11:13:35 +0800

    add tradeStateEnums

---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/enums/state/CloseStateEnum.java
@@ -0,0 +1,24 @@
+package study.daydayup.wolf.business.trade.api.enums.state;
+
+import lombok.Getter;
+import study.daydayup.wolf.common.lang.enums.CodeBasedEnum;
+
+/**
+ * study.daydayup.wolf.business.trade.api.enums
+ *
+ * @author Wingle
+ * @since 2019/10/5 6:55 PM
+ **/
+@Getter
+public enum CloseStateEnum implements CodeBasedEnum {
+    REFUND(20, "退款关闭"),
+    PAY_EXPIRED(10, "支付超时关闭")
+    ;
+
+    private int code;
+    private String desc;
+    CloseStateEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
