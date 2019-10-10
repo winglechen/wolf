deb936e9fb6c4681fb8d74719e587686daafd925
思源 <winglechen@gmail.com>
2019-10-10 09:59:38 +0800

    move state from order-biz to trade-api

---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/vo/state/ReceivedState.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.trade.api.vo.state;
+
+/**
+ * study.daydayup.wolf.business.trade.api.vo.state
+ *
+ * @author Wingle
+ * @since 2019/10/5 11:24 PM
+ **/
+public class ReceivedState extends TradeState {
+}
