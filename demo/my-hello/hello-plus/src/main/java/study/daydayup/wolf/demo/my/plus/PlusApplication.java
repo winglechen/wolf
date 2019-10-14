a08c425c2d12933aff4870645f7c8620f159551a
思源 <winglechen@gmail.com>
2019-10-14 15:13:35 +0800

    add mybatis-plus demo code

---
+++ demo/my-hello/hello-plus/src/main/java/study/daydayup/wolf/demo/my/plus/PlusApplication.java
@@ -0,0 +1,17 @@
+package study.daydayup.wolf.demo.my.plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * study.daydayup.wolf.demo.my.plus
+ *
+ * @author Wingle
+ * @since 2019/10/14 3:11 下午
+ **/
+@SpringBootApplication
+public class PlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlusApplication.class, args);
+    }
+}
