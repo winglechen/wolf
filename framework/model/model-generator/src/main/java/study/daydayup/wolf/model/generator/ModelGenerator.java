27c28324f1bf31baebca5918c6fbc4aa0645e133
思源 <winglechen@gmail.com>
2019-10-15 21:49:23 +0800

    add empty ModelGenerator

---
+++ framework/model/model-generator/src/main/java/study/daydayup/wolf/model/generator/ModelGenerator.java
@@ -0,0 +1,16 @@
+package study.daydayup.wolf.model.generator;
+
+/**
+ * study.daydayup.wolf.model.generator
+ *
+ * @author Wingle
+ * @since 2019/10/15 9:43 下午
+ **/
+public class ModelGenerator {
+    public static void generateDDL() {}
+    public static void generateDAO() {}
+
+    public static void generateDO() {}
+    public static void generateEntity() {}
+    public static void generateModel() {}
+}
