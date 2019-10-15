8817a7c86142c21964c8832aa76b450719efd8fb
思源 <winglechen@gmail.com>
2019-10-15 17:09:10 +0800

    add empty format package for model.string

---
+++ framework/model/model-engine/src/main/java/study/daydayup/wolf/model/type/string/format/Html.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.model.type.string.format;
+
+/**
+ * study.daydayup.wolf.model.type.string.format
+ *
+ * @author Wingle
+ * @since 2019/10/15 4:58 下午
+ **/
+public class Html {
+}
