53dc0b3c5e8b25f2ac20e30caed1e94e007ef587
思源 <winglechen@gmail.com>
2019-10-15 17:10:04 +0800

    add naming package

---
+++ framework/model/model-engine/src/main/java/study/daydayup/wolf/model/type/string/naming/Name.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.model.type.string.naming;
+
+import study.daydayup.wolf.model.contract.DataType;
+
+/**
+ * study.daydayup.wolf.model.type.string.naming
+ *
+ * @author Wingle
+ * @since 2019/10/15 4:58 下午
+ **/
+public class Name implements DataType {
+}
