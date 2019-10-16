7c5aa95fc09169b50a9b2868d0cdc8d5603b2a31
思源 <winglechen@gmail.com>
2019-10-16 10:01:46 +0800

    add empty length annotations

---
+++ framework/modeling/model/src/main/java/study/daydayup/wolf/model/annotation/length/MaxLen.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.model.annotation.length;
+
+/**
+ * study.daydayup.wolf.model.annotation.length
+ *
+ * @author Wingle
+ * @since 2019/10/16 9:49 上午
+ **/
+public @interface MaxLen {
+}
