a605b68674354764a37d23149d1186c6f32a28f6
思源 <winglechen@gmail.com>
2019-10-16 10:07:33 +0800

    rename Str to String

--- framework/modeling/model/src/main/java/study/daydayup/wolf/model/type/string/String.java
+++ framework/modeling/model/src/main/java/study/daydayup/wolf/model/type/string/String.java
@@ -8,5 +8,5 @@ import study.daydayup.wolf.model.contract.DataType;
  * @author Wingle
  * @since 2019/10/16 9:45 上午
  **/
-public class Str implements DataType {
+public class String implements DataType {
 }
