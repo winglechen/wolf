36913ea0e1d83228b2902fe3fd544484532f69f3
思源 <winglechen@gmail.com>
2019-10-16 10:02:18 +0800

    add OpenId datatype

---
+++ framework/modeling/model/src/main/java/study/daydayup/wolf/model/type/id/OpenId.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.model.type.id;
+
+import study.daydayup.wolf.model.contract.DataType;
+
+/**
+ * study.daydayup.wolf.model.type.id
+ *
+ * @author Wingle
+ * @since 2019/10/16 9:24 上午
+ **/
+public class OpenId implements DataType {
+}
