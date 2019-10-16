86c7f56feb9cc1eed2b0a4011ad25295fc51ca25
思源 <winglechen@gmail.com>
2019-10-16 10:03:44 +0800

    add Str dataType

---
+++ framework/modeling/model/src/main/java/study/daydayup/wolf/model/type/string/Str.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.model.type.string;
+
+import study.daydayup.wolf.model.contract.DataType;
+
+/**
+ * study.daydayup.wolf.model.type.string
+ *
+ * @author Wingle
+ * @since 2019/10/16 9:45 上午
+ **/
+public class Str implements DataType {
+}
