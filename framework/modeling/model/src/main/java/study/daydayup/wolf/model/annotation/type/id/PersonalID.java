be29035eac2273e4fffe93a0d94823a164294845
思源 <winglechen@gmail.com>
2019-10-17 15:43:36 +0800

    add empty annotations for model

---
+++ framework/modeling/model/src/main/java/study/daydayup/wolf/model/annotation/type/id/PersonalID.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.model.annotation.type.id;
+
+/**
+ * study.daydayup.wolf.model.annotation.type.id
+ *
+ * @author Wingle
+ * @since 2019/10/17 3:37 下午
+ **/
+public @interface PersonalID {
+}
