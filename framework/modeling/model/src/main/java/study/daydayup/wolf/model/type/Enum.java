54def229e3385552608089226ce88e0fd2e04915
思源 <winglechen@gmail.com>
2019-10-23 21:54:35 +0800

    add Enum type for model

---
+++ framework/modeling/model/src/main/java/study/daydayup/wolf/model/type/Enum.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.model.type;
+
+import study.daydayup.wolf.model.contract.DataType;
+
+/**
+ * study.daydayup.wolf.model.type
+ *
+ * @author Wingle
+ * @since 2019/10/18 6:00 下午
+ **/
+public class Enum implements DataType {
+}
