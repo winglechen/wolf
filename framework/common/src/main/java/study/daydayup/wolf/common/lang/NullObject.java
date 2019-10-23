ac53466b1996681ad1bc1cc91d53b884b38cbb0f
思源 <winglechen@gmail.com>
2019-10-23 21:53:32 +0800

    add NullObject for common

---
+++ framework/common/src/main/java/study/daydayup/wolf/common/lang/NullObject.java
@@ -0,0 +1,11 @@
+package study.daydayup.wolf.common.lang;
+
+/**
+ * study.daydayup.wolf.common.lang
+ * Just Replace the Null
+ *
+ * @author Wingle
+ * @since 2019/10/23 9:50 下午
+ **/
+public class NullObject {
+}
