9f8054fd8935abb7b6518cafa29123e16bda8c2c
思源 <winglechen@gmail.com>
2019-10-23 21:54:04 +0800

    add StockTypeEnum for goods.api

---
+++ business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/enums/StockTypeEnum.java
@@ -0,0 +1,29 @@
+package study.daydayup.wolf.business.goods.api.enums;
+
+import lombok.Getter;
+import study.daydayup.wolf.common.lang.enums.CodeBasedEnum;
+
+/**
+ * study.daydayup.wolf.business.goods.api.enums
+ *
+ * @author Wingle
+ * @since 2019/10/18 5:44 下午
+ **/
+@Getter
+public enum StockTypeEnum implements CodeBasedEnum {
+    TIMER_STOCK(60, "时段库存"),
+    TOKEN_STOCK(50, "令牌库存"),
+    NUMBER_STOCK(40, "数字库存"),
+    BOOL_STOCK(30, "是否库存"),
+    SOLD_STATE_STOCK(20, "上下架库存"),
+    NO_STOCK(10, "无库存")
+    ;
+
+    private int code;
+    private String desc;
+
+    StockTypeEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
