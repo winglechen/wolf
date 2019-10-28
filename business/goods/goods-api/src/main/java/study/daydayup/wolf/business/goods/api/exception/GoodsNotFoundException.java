c5ea172d55051dc939b66789526711e6d831a78f
思源 <winglechen@gmail.com>
2019-10-29 00:28:02 +0800

    add GoodsNotFoundException and GoodsWasDeletedException

---
+++ business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/exception/GoodsNotFoundException.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.goods.api.exception;
+
+/**
+ * study.daydayup.wolf.business.goods.api.exception
+ *
+ * @author Wingle
+ * @since 2019/10/29 12:13 上午
+ **/
+public class GoodsNotFoundException {
+}
