f3b0c2ac981c235423efc5126dc39317949ef30f
思源 <winglechen@gmail.com>
2019-10-29 00:00:33 +0800

    add hibernate-validation dependency for wolf-model

--- business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/entity/BaseGoods.java
+++ business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/entity/BaseGoods.java
@@ -2,6 +2,8 @@ package study.daydayup.wolf.business.goods.api.entity;
 
 import lombok.Data;
 import study.daydayup.wolf.framework.layer.api.Entity;
+import study.daydayup.wolf.model.annotation.key.BusinessKey;
+import study.daydayup.wolf.model.annotation.key.ForeignKey;
 
 /**
  * study.daydayup.wolf.business.goods.api.entity
@@ -11,7 +13,16 @@ import study.daydayup.wolf.framework.layer.api.Entity;
  **/
 @Data
 public class BaseGoods extends Entity {
+    @BusinessKey
     private long goodsId;
+    @ForeignKey
     private long orgId;
+    @ForeignKey
     private long categoryId;
+    /**
+     * @see study.daydayup.wolf.business.goods.api.enums.GoodsTypeEnum
+     */
+    private int goodsType;
+
+    private String name;
 }
