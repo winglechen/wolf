f891f29c9b218d6695af6b9aa01561a2d00da52c
思源 <winglechen@gmail.com>
2019-10-29 00:26:33 +0800

    add some properties for BaseGoods entity

--- business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/entity/BaseGoods.java
+++ business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/entity/BaseGoods.java
@@ -4,6 +4,10 @@ import lombok.Data;
 import study.daydayup.wolf.framework.layer.api.Entity;
 import study.daydayup.wolf.model.annotation.key.BusinessKey;
 import study.daydayup.wolf.model.annotation.key.ForeignKey;
+import study.daydayup.wolf.model.type.money.Money;
+import study.daydayup.wolf.model.type.string.Name;
+import study.daydayup.wolf.model.type.string.Tags;
+import study.daydayup.wolf.model.type.string.URI;
 
 /**
  * study.daydayup.wolf.business.goods.api.entity
@@ -19,10 +23,29 @@ public class BaseGoods extends Entity {
     private long orgId;
     @ForeignKey
     private long categoryId;
+
+    private Name name;
+    private Money price;
+
+    private Name feature;
+    private String vsPrice;
+    private URI mainPic;
+    private URI mainVideo;
+    private Tags tags;
+    private String code;
+    private int version;
+
     /**
      * @see study.daydayup.wolf.business.goods.api.enums.GoodsTypeEnum
      */
     private int goodsType;
+    /**
+     * @see study.daydayup.wolf.business.goods.api.enums.SoldStateEnum
+     */
+    private int soldState;
+    /**
+     * @see study.daydayup.wolf.business.goods.api.enums.StockTypeEnum
+     */
+    private int stockType;
 
-    private String name;
 }
