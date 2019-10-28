c341b896fcf6618f3972c1681f1bb2d2f5b0014c
思源 <winglechen@gmail.com>
2019-10-29 00:01:02 +0800

    add GoodsTypeEnum

---
+++ business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/enums/GoodsTypeEnum.java
@@ -0,0 +1,27 @@
+package study.daydayup.wolf.business.goods.api.enums;
+
+import lombok.Getter;
+import study.daydayup.wolf.common.lang.enums.CodeBasedEnum;
+
+/**
+ * study.daydayup.wolf.business.goods.api.enums
+ *
+ * @author Wingle
+ * @since 2019/10/18 5:44 下午
+ **/
+@Getter
+public enum GoodsTypeEnum implements CodeBasedEnum {
+    TICKET_GOODS(40, "票务商品"),
+    VIRTUAL_GOODS(30, "虚拟商品"),
+    IOT_GOODS(20, "IOT商品"),
+    NORMAL_GOODS(10, "普通商品")
+    ;
+
+    private int code;
+    private String desc;
+
+    GoodsTypeEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
