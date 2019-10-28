bbaefa7301a852d2b61da7eff42194b650d17186
思源 <winglechen@gmail.com>
2019-10-28 23:44:21 +0800

    add wolf.model dependency for wolf.boot

--- business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/entity/BaseGoods.java
+++ business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/entity/BaseGoods.java
@@ -1,5 +1,6 @@
 package study.daydayup.wolf.business.goods.api.entity;
 
+import lombok.Data;
 import study.daydayup.wolf.framework.layer.api.Entity;
 
 /**
@@ -8,5 +9,9 @@ import study.daydayup.wolf.framework.layer.api.Entity;
  * @author Wingle
  * @since 2019/10/3 11:00 PM
  **/
+@Data
 public class BaseGoods extends Entity {
+    private long goodsId;
+    private long orgId;
+    private long categoryId;
 }
