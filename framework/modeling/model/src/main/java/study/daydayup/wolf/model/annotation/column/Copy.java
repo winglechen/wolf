48aab181632b010b01154360d7d902ef67b29c4e
思源 <winglechen@gmail.com>
2019-10-29 12:10:42 +0800

    add empty annotation copy

---
+++ framework/modeling/model/src/main/java/study/daydayup/wolf/model/annotation/column/Copy.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.model.annotation.column;
+
+/**
+ * study.daydayup.wolf.model.annotation.column
+ *
+ * @author Wingle
+ * @since 2019/10/29 12:09 下午
+ **/
+public @interface Copy {
+}
