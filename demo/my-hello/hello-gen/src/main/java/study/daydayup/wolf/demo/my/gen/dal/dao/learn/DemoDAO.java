587dada5faa9cfef85a2d73513bfa34862da9639
思源 <winglechen@gmail.com>
2019-10-29 12:11:31 +0800

    add empty DemoDAO for demo.hello-gen

---
+++ demo/my-hello/hello-gen/src/main/java/study/daydayup/wolf/demo/my/gen/dal/dao/learn/DemoDAO.java
@@ -0,0 +1,17 @@
+package study.daydayup.wolf.demo.my.gen.dal.dao.learn;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+/**
+ * study.daydayup.wolf.demo.my.gen.dal.dao.learn
+ *
+ * @author Wingle
+ * @since 2019/10/29 10:13 上午
+ **/
+public class DemoDAO {
+
+
+    public static void main(String[] args) {
+    }
+}
