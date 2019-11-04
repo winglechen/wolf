eb9e10d71a7ca9210cea85fc13ff31ea2d61cb14
思源 <winglechen@gmail.com>
2019-11-04 19:30:12 +0800

    add dubbo generic reference demo code

--- demo/ali/hello-consumer/src/main/java/study/daydayup/wolf/demo/ali/consumer/GenericConsumer.java
+++ demo/ali/hello-consumer/src/main/java/study/daydayup/wolf/demo/ali/consumer/GenericConsumer.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GenericConsumer {
 
-    @RequestMapping("/generic")
+    @RequestMapping("/gen")
     public Object hello() {
         return "Hello";
     }
--- demo/ali/hello-consumer/src/main/java/study/daydayup/wolf/demo/ali/consumer/HelloConsumer.java
+++ demo/ali/hello-consumer/src/main/java/study/daydayup/wolf/demo/ali/consumer/HelloConsumer.java
@@ -1,6 +1,8 @@
 package study.daydayup.wolf.demo.ali.consumer;
 
+import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.annotation.Reference;
+import org.apache.dubbo.rpc.service.GenericService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.daydayup.wolf.demo.ali.api.service.HelloService;
@@ -19,5 +21,18 @@ public class HelloConsumer {
     @RequestMapping("/hello")
     public String hello() {
         return helloService.sayHello("wingle");
+    }
+
+    @RequestMapping("/generic")
+    public String generic() {
+        ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
+        reference.setInterface("study.daydayup.wolf.demo.ali.api.service.HelloService");
+        reference.setGeneric(true);
+
+        GenericService genericService = reference.get();
+        Object result = genericService.$invoke("sayHello", new String[]{"java.lang.String"}, new Object[]{"world"});
+
+
+        return  "generic result: " + (String) result;
     }
 }
