62c0cb42843341981a8a2e80385cc06b757ab4b4
思源 <winglechen@gmail.com>
2019-11-04 17:27:08 +0800

    move framework.layer.api.rpc out of api package

---
+++ framework/framework/src/main/java/study/daydayup/wolf/framework/layer/rpc/RpcService.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.framework.layer.rpc;
+
+/**
+ * study.daydayup.wolf.framework.layer.rpc
+ *
+ * @author Wingle
+ * @since 2019/9/29 4:42 PM
+ **/
+public @interface RpcService {
+}
