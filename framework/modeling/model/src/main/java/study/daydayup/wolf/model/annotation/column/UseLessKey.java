f7628710d29e77e0fe962571508c54222c01f7d2
思源 <winglechen@gmail.com>
2019-11-04 10:51:09 +0800

    add UselessKey

---
+++ framework/modeling/model/src/main/java/study/daydayup/wolf/model/annotation/column/UseLessKey.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.model.annotation.column;
+
+/**
+ * study.daydayup.wolf.model.annotation.column
+ *
+ * @author Wingle
+ * @since 2019/10/29 12:27 下午
+ **/
+public @interface UseLessKey {
+}
