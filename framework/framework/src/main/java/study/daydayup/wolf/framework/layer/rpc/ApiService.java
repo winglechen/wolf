1dee50afae0838bf6abee40f1c05c29b689a3bd0
思源 <winglechen@gmail.com>
2019-11-04 17:29:23 +0800

    add comment for RpcService

--- framework/framework/src/main/java/study/daydayup/wolf/framework/layer/rpc/ApiService.java
+++ framework/framework/src/main/java/study/daydayup/wolf/framework/layer/rpc/ApiService.java
@@ -2,6 +2,7 @@ package study.daydayup.wolf.framework.layer.rpc;
 
 /**
  * study.daydayup.wolf.framework.layer.rpc
+ * aliasFor RpcService
  *
  * @author Wingle
  * @since 2019/9/29 4:42 PM
--- framework/framework/src/main/java/study/daydayup/wolf/framework/layer/rpc/RpcService.java
+++ framework/framework/src/main/java/study/daydayup/wolf/framework/layer/rpc/RpcService.java
@@ -2,6 +2,7 @@ package study.daydayup.wolf.framework.layer.rpc;
 
 /**
  * study.daydayup.wolf.framework.layer.rpc
+ * 为协议支持提供可能(如：dubbo/spring cloud双协议支持)
  *
  * @author Wingle
  * @since 2019/9/29 4:42 PM
