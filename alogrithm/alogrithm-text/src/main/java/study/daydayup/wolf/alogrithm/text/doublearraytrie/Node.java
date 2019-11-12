3f0f1e849606a67493b7d5f3afb84a98c03055ef
思源 <winglechen@gmail.com>
2019-11-12 10:41:11 +0800

    add empty Node and DoubleArrayTrie

---
+++ alogrithm/alogrithm-text/src/main/java/study/daydayup/wolf/alogrithm/text/doublearraytrie/Node.java
@@ -0,0 +1,17 @@
+package study.daydayup.wolf.alogrithm.text.doublearraytrie;
+
+import lombok.Data;
+
+/**
+ * study.daydayup.wolf.alogrithm.text.doublearraytrie
+ *
+ * @author Wingle
+ * @since 2019/11/12 10:21 上午
+ **/
+@Data
+public class Node {
+    private int code;
+    private int depth;
+    private int left;
+    private int right;
+}
