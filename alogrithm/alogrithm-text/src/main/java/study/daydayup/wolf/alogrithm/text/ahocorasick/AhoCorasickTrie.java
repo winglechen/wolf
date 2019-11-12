1c71db6a86800286a4cc3e4fe3877114ca4c99fb
思源 <winglechen@gmail.com>
2019-11-12 11:27:02 +0800

    add empty aho corasick module

---
+++ alogrithm/alogrithm-text/src/main/java/study/daydayup/wolf/alogrithm/text/ahocorasick/AhoCorasickTrie.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.alogrithm.text.ahocorasick;
+
+/**
+ * study.daydayup.wolf.alogrithm.text.ahocorasick
+ *
+ * @author Wingle
+ * @since 2019/11/12 11:26 上午
+ **/
+public class AhoCorasickTrie {
+}
