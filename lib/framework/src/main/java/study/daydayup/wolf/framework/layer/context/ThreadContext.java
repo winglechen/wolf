7958ffc7e2a3606939638d5f539e4a1fc5756943
思源 <winglechen@gmail.com>
2019-11-15 13:55:36 +0800

    add empty ThreadContext

---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/layer/context/ThreadContext.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.framework.layer.context;
+
+/**
+ * study.daydayup.wolf.framework.layer.context
+ *
+ * @author Wingle
+ * @since 2019/11/15 1:54 下午
+ **/
+public class ThreadContext {
+}
