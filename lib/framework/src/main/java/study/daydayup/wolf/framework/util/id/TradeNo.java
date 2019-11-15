c293f321b9aa8620b7dbb1be0b1f731ebc5fb485
思源 <winglechen@gmail.com>
2019-11-15 15:33:04 +0800

    add TradeNo code

--- lib/framework/src/main/java/study/daydayup/wolf/framework/util/id/TradeNo.java
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/util/id/TradeNo.java
@@ -9,4 +9,53 @@ import study.daydayup.wolf.common.lang.string.ID;
  * @since 2019/11/15 2:48 下午
  **/
 public class TradeNo  implements ID {
+    public static final int ORDER       = 0 ;
+    public static final int CONTRACT    = 20;
+    public static final int PAY         = 30;
+
+
+    private int tradeType;
+    private int dataCenterNo;
+    private int shardingKey;
+    private int shardingNums;
+
+    public static TradeNo getInstance() {
+        return new TradeNo();
+    }
+
+    TradeNo() {
+        this.tradeType = 0;
+        this.dataCenterNo = 0;
+        this.shardingKey = 0;
+    }
+
+    public TradeNo tradeType(int tradeType) {
+        this.tradeType = tradeType;
+        return this;
+    }
+
+    public TradeNo dataCenterNo(int dataCenterNo) {
+        this.dataCenterNo = dataCenterNo;
+        return this;
+    }
+
+    public TradeNo shardingKey(int key) {
+        return this.shardingKey((long)key);
+    }
+
+    public TradeNo shardingKey(long key) {
+        this.shardingKey = 0;
+        return this;
+    }
+
+    public TradeNo shardingKey(String key) {
+
+        return this;
+    }
+
+    public  String build() {
+        return "";
+    }
+
+
 }
