d8baa1ed263f45ca17ef86b841b3b3491863c142
思源 <winglechen@gmail.com>
2019-11-15 14:56:35 +0800

    add TradeNo

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/lang/string/ID.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.common.lang.string;
+
+import java.io.Serializable;
+
+/**
+ * study.daydayup.wolf.common.lang.string
+ *
+ * @author Wingle
+ * @since 2019/11/15 2:54 下午
+ **/
+public interface ID extends Serializable {
+}
---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/util/id/TradeNo.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.framework.util.id;
+
+import study.daydayup.wolf.common.lang.string.ID;
+
+/**
+ * study.daydayup.wolf.framework.util.id
+ *
+ * @author Wingle
+ * @since 2019/11/15 2:48 下午
+ **/
+public class TradeNo  implements ID {
+}
