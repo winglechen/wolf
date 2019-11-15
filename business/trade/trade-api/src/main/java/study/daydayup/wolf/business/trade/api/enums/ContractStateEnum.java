3b3abbc9ee5aec3e3ecf6f548d0a3022a822c6d7
思源 <winglechen@gmail.com>
2019-11-15 10:19:17 +0800

    add TradeItemTypeEnum and TradeCategoryEnum

--- business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/enums/ContractStateEnum.java
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/enums/ContractStateEnum.java
@@ -10,14 +10,14 @@ import study.daydayup.wolf.common.lang.enums.CodeBasedEnum;
  * @since 2019/10/5 7:00 PM
  **/
 @Getter
-public enum ContractPhaseEnum implements CodeBasedEnum {
+public enum ContractStateEnum implements CodeBasedEnum {
     COMPLETED(100, "合同完成"),
     CREATED(10, "合同创建")
     ;
 
     private int code;
     private String desc;
-    ContractPhaseEnum(int code, String desc) {
+    ContractStateEnum(int code, String desc) {
         this.code = code;
         this.desc = desc;
     }
---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/enums/TradeItemTypeEnum.java
@@ -0,0 +1,25 @@
+package study.daydayup.wolf.business.trade.api.enums;
+
+import lombok.Getter;
+import study.daydayup.wolf.common.lang.enums.CodeBasedEnum;
+
+/**
+ * study.daydayup.wolf.business.trade.api.enums
+ *
+ * @author Wingle
+ * @since 2019/10/5 11:07 AM
+ **/
+@Getter
+public enum TradeItemTypeEnum implements CodeBasedEnum {
+    ORDER(20, "订单"),
+    CONTRACT(10, "合同"),
+    UNKNOWN(0, "未知交易"),
+    ;
+
+    private int code;
+    private String desc;
+    TradeItemTypeEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
