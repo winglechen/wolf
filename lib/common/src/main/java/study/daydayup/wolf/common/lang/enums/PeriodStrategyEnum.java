d618c87042b9c9eeb7bc81f2d7c8566d875a8dc0
思源 <winglechen@gmail.com>
2019-11-15 21:42:36 +0800

    add PeriodStrategyEnum

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/lang/enums/PeriodStrategyEnum.java
@@ -0,0 +1,27 @@
+package study.daydayup.wolf.common.lang.enums;
+
+import lombok.Getter;
+
+/**
+ * study.daydayup.wolf.common.lang.enums
+ * range(100~120)
+ *
+ * @author Wingle
+ * @since 2019/9/29 4:51 PM
+ **/
+@Getter
+public enum PeriodStrategyEnum implements CodeBasedEnum {
+    CLOSE_CLOSE(4, "close close"),
+    CLOSE_OPEN(3, "close open"),
+    OPEN_OPEN(2, "open open"),
+    OPEN_CLOSE(1, "open close")
+    ;
+
+    private int code;
+    private String desc;
+
+    PeriodStrategyEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+}
