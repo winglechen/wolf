51e7bb325351fd1bf4195ccb6baa8805f91a2bb9
思源 <winglechen@gmail.com>
2019-11-19 15:26:35 +0800

    add account deploy modules

---
+++ business/account/account-deploy-dubbo/src/main/java/study/daydayup/wolf/business/account/deploy/dubbo/AccountApplication.java
@@ -0,0 +1,17 @@
+package study.daydayup.wolf.business.account.deploy.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * study.daydayup.wolf.business.account.deploy.web
+ *
+ * @author Wingle
+ * @since 2019/9/29 2:13 PM
+ **/
+@SpringBootApplication
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
