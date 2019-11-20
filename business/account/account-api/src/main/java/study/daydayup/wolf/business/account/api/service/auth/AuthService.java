50650cf0505b62232935bbb864f962a1ccaa37b6
思源 <winglechen@gmail.com>
2019-11-20 14:25:06 +0800

    add AuthService

---
+++ business/account/account-api/src/main/java/study/daydayup/wolf/business/account/api/service/auth/AuthService.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.account.api.service.auth;
+
+import study.daydayup.wolf.business.account.api.entity.License;
+
+/**
+ * study.daydayup.wolf.business.account.api.service.auth
+ *
+ * @author Wingle
+ * @since 2019/9/27 5:37 PM
+ **/
+public interface AuthService {
+    boolean logout();
+}
