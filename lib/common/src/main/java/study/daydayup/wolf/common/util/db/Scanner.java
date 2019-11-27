26522a5a1a3efd97d4dd0803ee7291aea040d7a0
思源 <winglechen@gmail.com>
2019-11-27 11:41:21 +0800

    add scanner

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/util/db/Scanner.java
@@ -0,0 +1,17 @@
+package study.daydayup.wolf.common.util.db;
+
+/**
+ * study.daydayup.wolf.common.util.db
+ *
+ * @author Wingle
+ * @since 2019/11/27 9:33 上午
+ **/
+public class Scanner {
+    public Scanner scan(String table, String columns) {
+        return this;
+    }
+
+    public Scanner to() {
+        return this;
+    }
+}
