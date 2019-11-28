6dc224f1e5b2886f70dcdabb9c963606b906c45f
思源 <winglechen@gmail.com>
2019-11-28 17:39:23 +0800

    rename module estar to onion

--- app/onion/onion-api/src/main/java/study/daydayup/wolf/demo/estar/api/account/service/UserService.java
+++ app/onion/onion-api/src/main/java/study/daydayup/wolf/demo/estar/api/account/service/UserService.java
@@ -1,7 +1,7 @@
-package study.daydayup.wolf.demo.estar.api.account.service;
+package study.daydayup.wolf.demo.onion.api.account.service;
 
 /**
- * study.daydayup.wolf.demo.estar.api.account.service
+ * study.daydayup.wolf.demo.onion.api.account.service
  *
  * @author Wingle
  * @since 2019/11/19 1:24 下午
