b14569a48b0944d4bab1f6d93a0bdb7deb0bb0e5
smallSun530 <454502428@qq.com>
2019-11-29 01:16:13 +0800

    优化sql

--- docs/databases/auth.sql
+++ docs/databases/auth.sql
@@ -3,27 +3,20 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin`
 (
     `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `account`     VARCHAR(50)         NOT NULL DEFAULT '' COMMENT '密码',
-    `password`    VARCHAR(64)         NOT NULL DEFAULT '' COMMENT '密码',
-    `salt`        VARCHAR(50)         NOT NULL DEFAULT '' COMMENT '盐值',
-    `mobile`      VARCHAR(20)         NOT NULL DEFAULT '' COMMENT '手机号',
-    `real_name`   VARCHAR(100)        NOT NULL DEFAULT '' COMMENT '姓名',
-    `nickname`    VARCHAR(100)        NOT NULL DEFAULT '' COMMENT '用户名',
-    `address`     VARCHAR(500)        NOT NULL DEFAULT '' COMMENT '住址',
-    `gender`      TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '性别 0:male(男) 1:female(女)',
+    `org_id`      BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '组织id',
     `account_id`  BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账号id',
-    `org_id`      BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '组织ID',
+    `username`    VARCHAR(100)        NOT NULL DEFAULT '' COMMENT '用户名',
+    `nickname`    VARCHAR(100)        NOT NULL DEFAULT '' COMMENT '昵称',
+    `mobile`      VARCHAR(20)         NOT NULL DEFAULT '' COMMENT '手机号',
     `status`      BIGINT(2) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '用户状态 1正常 2禁用 3账户锁定',
     `delete_flag` TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除 0未删除，1已删除',
+    `version`     INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
     `last_editor` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
     `created_at`  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updated_at`  DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
-    UNIQUE INDEX `udx_account` (`account`),
-    UNIQUE INDEX `udx_mobile` (`mobile`),
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4 COMMENT = '后台用户管理信息表';
-
 
 
 -- 1.角色信息表
@@ -32,6 +25,7 @@ CREATE TABLE IF NOT EXISTS `role`
 (
     `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
     `role_name`   VARCHAR(50)         NOT NULL DEFAULT '' COMMENT '角色名称',
+    `version`     INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
     `last_editor` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
     `created_at`  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updated_at`  DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
@@ -41,12 +35,14 @@ CREATE TABLE IF NOT EXISTS `role`
 
 
 -- 2.用户角色关联信息表
-DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE IF NOT EXISTS `user_role`
+DROP TABLE IF EXISTS `admin_role`;
+CREATE TABLE IF NOT EXISTS `admin_role`
 (
     `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `uid`         BIGINT(20)          NOT NULL DEFAULT '' COMMENT '用户id',
+    `org_id`      BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '组织id',
+    `account_id`  BIGINT(20)          NOT NULL DEFAULT '' COMMENT '用户id',
     `role_id`     BIGINT(20)          NOT NULL DEFAULT '' COMMENT '角色id',
+    `version`     INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
     `last_editor` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
     `created_at`  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updated_at`  DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
@@ -59,8 +55,11 @@ DROP TABLE IF EXISTS `permission`;
 CREATE TABLE IF NOT EXISTS `permission`
 (
     `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `org_type`    INT(11)             NOT NULL DEFAULT 0 COMMENT '组织类型',
+    `parent_id`   BIGINT(20)          NOT NULL DEFAULT 0 COMMENT '父权限id',
     `name`        VARCHAR(100)        NOT NULL DEFAULT '' COMMENT '资源名称',
     `code`        VARCHAR(50)         NOT NULL DEFAULT '' COMMENT '权限代码字符串',
+    `version`     INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
     `last_editor` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
     `created_at`  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `updated_at`  DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
@@ -72,14 +71,13 @@ CREATE TABLE IF NOT EXISTS `permission`
 DROP TABLE IF EXISTS `role_permission`;
 CREATE TABLE IF NOT EXISTS `role_permission`
 (
-    `id`           BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `role_id` BIGINT(20) NOT NULL DEFAULT 0 COMMENT '角色id',
-    `permission_id` BIGINT(20) NOT NULL DEFAULT 0 COMMENT '权限id',
-
-    `last_editor` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
-    `created_at`  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_at`  DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
+    `id`            BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `role_id`       BIGINT(20)          NOT NULL DEFAULT 0 COMMENT '角色id',
+    `permission_id` BIGINT(20)          NOT NULL DEFAULT 0 COMMENT '权限id',
+    `version`       INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
+    `last_editor`   BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
+    `created_at`    DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`    DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4 COMMENT = '角色权限信息表';
-
--- docs/databases/ovderdue_case.sql
+++ docs/databases/ovderdue_case.sql
@@ -1,125 +1,71 @@
--- 1.逾期订单案件表
+-- 1.催收任务信息表
 DROP TABLE IF EXISTS `overdue_case`;
 CREATE TABLE IF NOT EXISTS `overdue_case`
 (
-    `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `order_id`    BIGINT(20)          NOT NULL DEFAULT 0 COMMENT '逾期订单id',
-    `status`      TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '案件处理状态 0未处理 2处理中 3案件完成',
-    `account_id`  BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账号id',
-    `org_id`      BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '组织ID',
-    `version`     INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
-    `last_editor` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
-    `created_at`  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_at`  DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARACTER SET = utf8mb4 COMMENT = '逾期订单案件表';
+    `id`              BIGINT(20) UNSIGNED  NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `org_id`          BIGINT(20) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '组织id',
+    `account_id`      BIGINT(20) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '账号id',
+    `assigner_id`     BIGINT(20) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '任务分配人',
 
--- 2.催收员信息表
-# DROP TABLE IF EXISTS `collector`;
-# CREATE TABLE IF NOT EXISTS `collector`
-# (
-#     `id`               BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-#     `name`             BIGINT(20)          NOT NULL DEFAULT 0 COMMENT '催收员姓名',
-#     `mobile`           BIGINT(20)          NOT NULL DEFAULT 0 COMMENT '催收员手机号',
-#     `status`           TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '状态 0正常 1禁用',
-#     `delete_flag`      TINYINT(3) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除 0未删除，1已删除',
-#     `account_id`       BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账号id',
-#     `org_id`           BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '组织ID',
-#     `collector_org_id` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收员组织ID',
-#     `version`          INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
-#     `last_editor`      BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
-#     `created_at`       DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-#     `updated_at`       DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
-#     PRIMARY KEY (`id`)
-# ) ENGINE = InnoDB
-#   DEFAULT CHARACTER SET = utf8mb4 COMMENT = '催收员信息表';
+    `contract_no`     VARCHAR(30)          NOT NULL DEFAULT '' COMMENT '合同号',
+    `trade_no`        VARCHAR(30)          NOT NULL DEFAULT '' COMMENT '交易号',
+    `installment_no`  SMALLINT(6) UNSIGNED NOT NULL DEFAULT 0 COMMENT '分期号',
+    `case_account_id` BIGINT(20) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '案件人id',
+    `case_org_id`     BIGINT(20) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '案件人的组织id',
+    `status`          TINYINT(2) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '案件处理状态 0待处理 1处理中 2案件完成',
+    `memo`            VARCHAR(200)         NOT NULL DEFAULT '' COMMENT '催收情况描述',
 
-
--- 3.催收任务信息表
-DROP TABLE IF EXISTS `collection`;
-CREATE TABLE IF NOT EXISTS `collection`
-(
-    `id`               BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `order_id`         BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '预期订单id',
-    `collector_id`     BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收员id',
-    `account_id`       BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账号id',
-    `org_id`           BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '逾期订单组织ID',
-    `collector_org_id` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收员组织ID',
-    `version`          INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
-    `status`           TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收结果状态 1催收失败 2催收中 3催收成功',
-    `describe`         VARCHAR(200)        NOT NULL DEFAULT '' COMMENT '催收情况描述',
-    `uid`    BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '任务分配人',
-    `last_editor`      BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
-    `created_at`       DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_at`       DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
+    `delete_flag`     TINYINT(2) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '是否删除 0未删除，1已删除',
+    `version`         INT(11) UNSIGNED     NOT NULL DEFAULT 0 COMMENT '版本号',
+    `last_editor`     BIGINT(20) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '最后编辑者',
+    `created_at`      DATETIME             NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`      DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4 COMMENT = '催收任务信息表';
 
 
--- 4.催收日志信息表
-DROP TABLE IF EXISTS `collection_log`;
-CREATE TABLE IF NOT EXISTS `collection_log`
+-- 2.催收日志信息表
+DROP TABLE IF EXISTS `collection`;
+CREATE TABLE IF NOT EXISTS `collection`
 (
-    `id`              BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `order_id`        BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收订单id',
-    `collection_id`   BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收任务id',
-    `collector_id`    BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收员id',
-    `proof_img`       VARCHAR(200)        NOT NULL DEFAULT '' COMMENT '催收凭证url/img',
-    `describe`        VARCHAR(200)        NOT NULL DEFAULT '' COMMENT '催收情况描述',
-    `repay_method`    TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '回款还款方式 0线上 1线下',
-    `repay_money`     DECIMAL(10, 2)      NOT NULL DEFAULT 0 COMMENT '回款还款金额',
-    `repay_status`    TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '回款还款状态 0未还款 1.部分还款 2.结清',
-    `repay_proof_img` VARCHAR(200)        NOT NULL DEFAULT '' COMMENT '还款凭证url/img',
-    `last_editor`     BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
-    `created_at`      DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_at`      DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
+    `id`                BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `org_id`            BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '组织id',
+    `account_id`        BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收员id',
+    `case_id`           BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收任务id',
+    `collection_method` TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收方式 1短信 2电信 3.微信 4.其他',
+    `contacts`          VARCHAR(100)        NOT NULL DEFAULT '' COMMENT '催款联系人',
+    `contacts_status`   TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '联系状态 1联系不上 2承诺还款 3.延期 4.拒绝还款',
+    `promise_pay_at`    DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '承诺还款时间',
+    `repay_status`      TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '回款还款状态 0未还款 1.部分还款 2.结清',
+    `repay_method`      TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '回款还款方式 0线上 1线下',
+    `repay_money`       BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '回款还款金额',
+    `describe`          VARCHAR(200)        NOT NULL DEFAULT '' COMMENT '催收情况描述',
+    `collection_proof`  VARCHAR(1000)       NOT NULL DEFAULT '' COMMENT '催收凭证url/img',
+    `repay_proof`       VARCHAR(1000)       NOT NULL DEFAULT '' COMMENT '还款凭证url/img',
+
+    `delete_flag`       TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除 0未删除，1已删除',
+    `version`           INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
+    `last_editor`       BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
+    `created_at`        DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`        DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4 COMMENT = '催收任日志信息表';
 
 
--- 5.组织催收关联关系表
-DROP TABLE IF EXISTS `organization_collection`;
-CREATE TABLE IF NOT EXISTS `organization_collection`
+-- 3.组织催收关联关系表
+DROP TABLE IF EXISTS `org_collection`;
+CREATE TABLE IF NOT EXISTS `org_collection`
 (
-    `id`               BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `account_id`       BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账号id',
-    `org_id`           BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '订单组织ID',
-    `collector_org_id` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收组织ID',
-    `type`             TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '关系类型：0催收 1.  2.',
-    `version`          INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
-    `last_editor`      BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
-    `created_at`       DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_at`       DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
+    `id`                BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `org_id`            BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '组织id',
+    `collection_org_id` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '催收组织id',
+    `delete_flag`       TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除 0未删除，1已删除',
+    `version`           INT(11) UNSIGNED    NOT NULL DEFAULT 0 COMMENT '版本号',
+    `last_editor`       BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
+    `created_at`        DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`        DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4 COMMENT = '组织催收关联关系表';
-
-
--- 6.后台用户管理信息表
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin`
-(
-    `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
-    `account`     VARCHAR(50)         NOT NULL DEFAULT '' COMMENT '密码',
-    `password`    VARCHAR(64)         NOT NULL DEFAULT '' COMMENT '密码',
-    `salt`        VARCHAR(50)         NOT NULL DEFAULT '' COMMENT '盐值',
-    `mobile`      VARCHAR(20)         NOT NULL DEFAULT '' COMMENT '手机号',
-    `real_name`   VARCHAR(100)        NOT NULL DEFAULT '' COMMENT '姓名',
-    `nickname`    VARCHAR(100)        NOT NULL DEFAULT '' COMMENT '用户名',
-    `address`     VARCHAR(500)        NOT NULL DEFAULT '' COMMENT '住址',
-    `gender`      TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '性别 0:male(男) 1:female(女)',
-    `account_id`  BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账号id',
-    `org_id`      BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '组织ID',
-    `status`      BIGINT(2) UNSIGNED  NOT NULL DEFAULT 0 COMMENT '用户状态 1正常 2禁用 3账户锁定',
-    `delete_flag` TINYINT(2) UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否删除 0未删除，1已删除',
-    `last_editor` BIGINT(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '最后编辑者',
-    `created_at`  DATETIME            NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_at`  DATETIME ON UPDATE CURRENT_TIMESTAMP COMMENT '编辑时间',
-    UNIQUE INDEX `udx_account` (`account`),
-    UNIQUE INDEX `udx_mobile` (`mobile`),
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  DEFAULT CHARACTER SET = utf8mb4 COMMENT = '后台用户管理信息表';
