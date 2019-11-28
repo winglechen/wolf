a23f84ac60e9d20df076f070070f2133443ffaec
思源 <winglechen@gmail.com>
2019-11-28 20:14:58 +0800

    add mq code

---
+++ middleware/mq/client/src/main/java/study/daydayup/wolf/mq/client/entity/Message.java
@@ -0,0 +1,18 @@
+package study.daydayup.wolf.mq.client.entity;
+
+import lombok.Data;
+
+/**
+ * study.daydayup.wolf.mq.client.message
+ *
+ * @author Wingle
+ * @since 2019/11/28 6:52 下午
+ **/
+@Data
+public class Message {
+    private long id;
+    private String topic;
+    private String messageId;
+    private String tags;
+    private Object message;
+}
