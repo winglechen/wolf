a3177976532e42ea7a151373319e1df81991785e
思源 <winglechen@gmail.com>
2019-12-10 16:33:14 +0800

    add union deploy modules

---
+++ business/union/union-deploy-dubbo/src/main/java/study/daydayup/wolf/business/union/deploy/dubbo/DubboUnionApplication.java
@@ -0,0 +1,21 @@
+package study.daydayup.wolf.business.union.deploy.dubbo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * study.daydayup.wolf.business.account.deploy.web
+ *
+ * @author Wingle
+ * @since 2019/9/29 2:13 PM
+ **/
+@SpringBootApplication(scanBasePackages = {"study.daydayup.wolf.business.account"})
+@MapperScan({"study.daydayup.wolf.business.account.biz.dal.dao"})
+@EnableDiscoveryClient
+public class DubboUnionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboUnionApplication.class, args);
+    }
+}
---
+++ business/union/union-deploy-web/src/main/java/study/daydayup/wolf/business/union/deploy/web/WebUnionApplication.java
@@ -0,0 +1,19 @@
+package study.daydayup.wolf.business.union.deploy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * study.daydayup.wolf.business.account.deploy.web
+ *
+ * @author Wingle
+ * @since 2019/9/29 2:13 PM
+ **/
+@SpringBootApplication(scanBasePackages = {"study.daydayup.wolf.business.account"})
+@EnableDiscoveryClient
+public class WebUnionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebUnionApplication.class, args);
+    }
+}
