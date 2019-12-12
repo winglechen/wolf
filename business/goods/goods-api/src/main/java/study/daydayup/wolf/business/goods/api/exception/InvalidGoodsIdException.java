a18a2457ff61d14472b9739597ca54178dfb7326
思源 <winglechen@gmail.com>
2019-12-12 14:26:16 +0800

    add InvalidGoodsIdException

---
+++ business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/exception/InvalidGoodsIdException.java
@@ -0,0 +1,15 @@
+package study.daydayup.wolf.business.goods.api.exception;
+
+import study.daydayup.wolf.common.lang.exception.SystemException;
+
+/**
+ * study.daydayup.wolf.business.goods.api.exception
+ *
+ * @author Wingle
+ * @since 2019/12/12 2:04 下午
+ **/
+public class InvalidGoodsIdException extends SystemException {
+    public InvalidGoodsIdException(long goodsId) {
+        super(140004, "Invalid goodsId: " + goodsId);
+    }
+}
