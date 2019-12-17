1b418696a5a7c8c6a2dc314094a3c371bdd941a0
思源 <winglechen@gmail.com>
2019-12-17 17:48:54 +0800

    add TradeStateMap interface and DefaultTradeStateMap

---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/exception/TradeStateNotFoundException.java
@@ -0,0 +1,16 @@
+package study.daydayup.wolf.business.trade.api.exception;
+
+import study.daydayup.wolf.common.lang.exception.SystemException;
+import study.daydayup.wolf.common.lang.string.Msg;
+
+/**
+ * study.daydayup.wolf.business.trade.api.exception
+ *
+ * @author Wingle
+ * @since 2019/12/16 7:48 下午
+ **/
+public class TradeStateNotFoundException extends SystemException {
+    public TradeStateNotFoundException(int state) {
+        super(160004, Msg.join("Invalid trade state: ", state));
+    }
+}
---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/state/loan/OverduePaidState.java
@@ -0,0 +1,17 @@
+package study.daydayup.wolf.business.trade.api.state.loan;
+
+import lombok.Data;
+import study.daydayup.wolf.business.trade.api.state.AbstractTradeState;
+import study.daydayup.wolf.business.trade.api.state.TradeState;
+
+/**
+ * study.daydayup.wolf.business.trade.api.state
+ *
+ * @author Wingle
+ * @since 2019/10/5 11:23 PM
+ **/
+@Data
+public class OverduePaidState extends AbstractTradeState implements TradeState {
+    protected int code = 201;
+    protected String name = "逾期还款成功";
+}
