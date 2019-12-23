9d5b9642637898592d7144c6e9806904ab6f1979
思源 <winglechen@gmail.com>
2019-12-23 11:43:37 +0800

    add empty trade node

---
+++ business/trade/buy-biz/src/main/java/study/daydayup/wolf/business/trade/buy/biz/loan/node/FirstLoanCheckNode.java
@@ -0,0 +1,23 @@
+package study.daydayup.wolf.business.trade.buy.biz.loan.node;
+
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.trade.buy.biz.common.TradeNode;
+import study.daydayup.wolf.business.trade.buy.biz.common.context.BuyContext;
+import study.daydayup.wolf.business.trade.buy.biz.common.node.AbstractTradeNode;
+
+/**
+ * study.daydayup.wolf.business.trade.buy.biz.loan.node
+ *
+ * @author Wingle
+ * @since 2019/12/17 8:19 下午
+ **/
+@Component
+public class FirstLoanCheckNode extends AbstractTradeNode implements TradeNode {
+
+    @Override
+    public void run(BuyContext context) {
+        init(context);
+
+    }
+
+}
