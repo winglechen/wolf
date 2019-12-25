dc89b36e8bf5f991de5a33d24617b82f5fb350be
思源 <winglechen@gmail.com>
2019-12-25 10:38:24 +0800

    add Event and State interface for StateMachine

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/sm/State.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.common.sm;
+
+import java.io.Serializable;
+
+/**
+ * study.daydayup.common.framework.layer.domain
+ *
+ * @author Wingle
+ * @since 2018/12/26 11:45 PM
+ **/
+public interface State extends Serializable {
+    int getCode();
+}
