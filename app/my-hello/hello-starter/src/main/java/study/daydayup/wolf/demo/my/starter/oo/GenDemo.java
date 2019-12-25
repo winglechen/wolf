deb24490f327842b9e0bb10cc4c1ba0cc7386570
思源 <winglechen@gmail.com>
2019-12-25 21:51:17 +0800

    add leaning code

---
+++ app/my-hello/hello-starter/src/main/java/study/daydayup/wolf/demo/my/starter/oo/GenDemo.java
@@ -0,0 +1,46 @@
+package study.daydayup.wolf.demo.my.starter.oo;
+
+import lombok.Data;
+
+/**
+ * study.daydayup.wolf.demo.my.starter.oo
+ *
+ * @author Wingle
+ * @since 2019/12/25 8:26 下午
+ **/
+public class GenDemo {
+
+    public static void main(String[] args) {
+        Order o = new Order();
+
+        Child c = new Child(o);
+        System.out.println(c.show());
+    }
+
+    @Data
+    static class Parent<T> {
+        protected T model;
+
+        protected void init(T model) {
+            this.model = model;
+        }
+    }
+
+    @Data
+    static class Child extends Parent<Order> {
+        private String name;
+
+        public Child(Order o) {
+            init(o);
+        }
+
+        public String show() {
+            return this.model.getName();
+        }
+    }
+
+    @Data
+    static class Order {
+        private String name = "order";
+    }
+}
