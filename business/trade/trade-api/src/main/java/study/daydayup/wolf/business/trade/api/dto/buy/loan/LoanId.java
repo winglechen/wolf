cf0c76b47b38c49003b7c88cac7b3b6ffde22f82
思源 <winglechen@gmail.com>
2019-12-25 21:51:48 +0800

    add LoanService

---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/dto/buy/loan/LoanId.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.trade.api.dto.buy.loan;
+
+import study.daydayup.wolf.business.trade.api.dto.buy.TradeId;
+import study.daydayup.wolf.framework.layer.api.Request;
+
+/**
+ * study.daydayup.wolf.business.trade.api.dto.buy.loan
+ *
+ * @author Wingle
+ * @since 2019/12/25 9:32 下午
+ **/
+public class LoanId extends TradeId implements Request {
+}
