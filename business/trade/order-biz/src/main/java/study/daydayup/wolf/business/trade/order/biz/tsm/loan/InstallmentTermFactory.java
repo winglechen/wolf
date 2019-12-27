4f5c728346cd3b6856848b880b9abf88d26e7431
思源 <winglechen@gmail.com>
2019-12-27 17:08:58 +0800

    add InstallmentTerm state machine Factory

--- business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/biz/tsm/loan/InstallmentTermFactory.java
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/biz/tsm/loan/InstallmentTermFactory.java
@@ -1,8 +1,27 @@
 package study.daydayup.wolf.business.trade.order.biz.tsm.loan;
 
 import study.daydayup.wolf.business.trade.api.event.TradeEvent;
+import study.daydayup.wolf.business.trade.api.event.base.CompleteEvent;
+import study.daydayup.wolf.business.trade.api.event.loan.MarkLossEvent;
+import study.daydayup.wolf.business.trade.api.event.loan.collection.ConfirmCollectionEvent;
+import study.daydayup.wolf.business.trade.api.event.loan.loan.LoanSuccessEvent;
+import study.daydayup.wolf.business.trade.api.event.loan.repay.PrepayInstallmentEvent;
+import study.daydayup.wolf.business.trade.api.event.loan.repay.RepayDueEvent;
+import study.daydayup.wolf.business.trade.api.event.loan.repay.RepayOverdueEvent;
+import study.daydayup.wolf.business.trade.api.event.loan.repay.RepaySuccessEvent;
 import study.daydayup.wolf.business.trade.api.state.TradeState;
+import study.daydayup.wolf.business.trade.api.state.base.CompletedState;
+import study.daydayup.wolf.business.trade.api.state.base.PaidState;
+import study.daydayup.wolf.business.trade.api.state.loan.CloseAsLossState;
+import study.daydayup.wolf.business.trade.api.state.loan.OverduePaidState;
+import study.daydayup.wolf.business.trade.api.state.loan.PrepaidState;
+import study.daydayup.wolf.business.trade.api.state.loan.collection.PaidByCollectionState;
+import study.daydayup.wolf.business.trade.api.state.loan.repay.DueState;
+import study.daydayup.wolf.business.trade.api.state.loan.repay.EffectedState;
+import study.daydayup.wolf.business.trade.api.state.loan.repay.OverdueState;
+import study.daydayup.wolf.business.trade.api.state.loan.repay.WaitToEffectState;
 import study.daydayup.wolf.business.trade.order.biz.tsm.TradeStateMachineFactory;
+import study.daydayup.wolf.common.sm.DefaultStateMachine;
 import study.daydayup.wolf.common.sm.StateMachine;
 
 /**
@@ -12,8 +31,40 @@ import study.daydayup.wolf.common.sm.StateMachine;
  * @since 2019/12/26 7:11 下午
  **/
 public class InstallmentTermFactory implements TradeStateMachineFactory {
+    private TradeState waitToEffect         = new WaitToEffectState();
+    private TradeState effected             = new EffectedState();
+    private TradeState due                  = new DueState();
+    private TradeState overdue              = new OverdueState();
+    private TradeState paid                 = new PaidState();
+    private TradeState prepaid              = new PrepaidState();
+
+    private TradeState overduePaid          = new OverduePaidState();
+    private TradeState paidByCollection     = new PaidByCollectionState();
+    private TradeState closeAsLoss          = new CloseAsLossState();
+
+    private TradeState completed            = new CompletedState();
+
     @Override
     public StateMachine<TradeState, TradeEvent> create() {
-        return null;
+        return new DefaultStateMachine<TradeState, TradeEvent>(waitToEffect)
+                .bind(waitToEffect, effected, new LoanSuccessEvent())
+
+                .bind(effected, due, new RepayDueEvent())
+                .bind(effected, overdue, new RepayOverdueEvent())
+                .bind(due, overdue, new RepayOverdueEvent())
+
+                .bind(effected, paid, new RepaySuccessEvent())
+                .bind(effected, prepaid, new PrepayInstallmentEvent())
+                .bind(due, paid, new RepaySuccessEvent())
+                .bind(due, prepaid, new PrepayInstallmentEvent())
+
+                .bind(overdue, overduePaid, new RepayOverdueEvent())
+                .bind(overdue, paidByCollection, new ConfirmCollectionEvent())
+                .bind(overdue, closeAsLoss, new MarkLossEvent())
+
+                .bind(paid, completed, new CompleteEvent())
+                .bind(prepaid, completed, new CompleteEvent())
+
+                ;
     }
 }
--- business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/biz/tsm/loan/LoanContractFactory.java
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/biz/tsm/loan/LoanContractFactory.java
@@ -1,7 +1,10 @@
 package study.daydayup.wolf.business.trade.order.biz.tsm.loan;
 
 import study.daydayup.wolf.business.trade.api.event.TradeEvent;
+import study.daydayup.wolf.business.trade.api.event.base.CompleteEvent;
 import study.daydayup.wolf.business.trade.api.event.loan.*;
+import study.daydayup.wolf.business.trade.api.event.loan.collection.ConfirmCollectionEvent;
+import study.daydayup.wolf.business.trade.api.event.loan.repay.PrepayAllEvent;
 import study.daydayup.wolf.business.trade.api.event.loan.repay.RepayBeginEvent;
 import study.daydayup.wolf.business.trade.api.event.loan.repay.RepayOverdueEvent;
 import study.daydayup.wolf.business.trade.api.event.loan.repay.RepaySuccessEvent;
@@ -10,6 +13,7 @@ import study.daydayup.wolf.business.trade.api.event.loan.loan.LoanSuccessEvent;
 import study.daydayup.wolf.business.trade.api.state.TradeState;
 import study.daydayup.wolf.business.trade.api.state.base.CompletedState;
 import study.daydayup.wolf.business.trade.api.state.loan.*;
+import study.daydayup.wolf.business.trade.api.state.loan.collection.PaidByCollectionState;
 import study.daydayup.wolf.business.trade.api.state.loan.contract.*;
 import study.daydayup.wolf.business.trade.api.state.loan.repay.OverdueState;
 import study.daydayup.wolf.business.trade.order.biz.tsm.TradeStateMachineFactory;
@@ -33,14 +37,20 @@ public class LoanContractFactory implements TradeStateMachineFactory {
 
     private TradeState repaying             = new RepayingState();
     private TradeState overdue              = new OverdueState();
+
     private TradeState repaid               = new RepaidState();
+    private TradeState prepaid              = new PrepaidState();
 
     private TradeState completed            = new CompletedState();
     private TradeState overduePaid          = new OverduePaidState();
+    private TradeState paidByCollection     = new PaidByCollectionState();
+
     private TradeState refused              = new RefusedState();
+    private TradeState closeAsLoss          = new CloseAsLossState();
 
     @Override
     public StateMachine<TradeState, TradeEvent> create() {
+        //TODO: Prepay support
         machine = new DefaultStateMachine<TradeState, TradeEvent>(waitToApprove)
                 .bind(waitToApprove, approved, new ApproveEvent())
                 .bind(waitToApprove, refused, new RefuseEvent())
@@ -51,12 +61,19 @@ public class LoanContractFactory implements TradeStateMachineFactory {
 
                 .bind(loaned, repaying, new RepayBeginEvent())
                 .bind(loaned, overdue, new RepayOverdueEvent())
-                .bind(loaned, completed, new RepaySuccessEvent())
+                .bind(loaned, repaid, new RepaySuccessEvent())
+                .bind(loaned, prepaid, new PrepayAllEvent())
 
                 .bind(repaying, overdue, new RepayOverdueEvent())
-                .bind(repaying, completed, new RepaySuccessEvent())
+                .bind(repaying, repaid, new RepaySuccessEvent())
+                .bind(repaying, prepaid, new PrepayAllEvent())
 
                 .bind(overdue, overduePaid, new RepayOverdueEvent())
+                .bind(overdue, paidByCollection, new ConfirmCollectionEvent())
+                .bind(overdue, closeAsLoss, new MarkLossEvent())
+
+                .bind(repaid, completed, new CompleteEvent())
+                .bind(prepaid, completed, new CompleteEvent())
                 ;
 
         return machine;
