6cd468dc8c6b2b6eea5eec69e94623d6a4f10f93
思源 <winglechen@gmail.com>
2019-12-31 18:19:25 +0800

    add some trade request

---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/dto/tm/SellerRequest.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.business.trade.api.dto.tm;
+
+import study.daydayup.wolf.framework.layer.api.Request;
+
+/**
+ * study.daydayup.wolf.business.trade.api.dto.tm
+ *
+ * @author Wingle
+ * @since 2019/12/31 2:23 上午
+ **/
+public class SellerRequest implements Request {
+}
