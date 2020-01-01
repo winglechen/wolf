e8ff7fff1308dbf0defbbc1235028ad800b87863
思源 <winglechen@gmail.com>
2020-01-01 17:27:57 +0800

    rename union-api to union-admin

---
+++ business/union/union-deploy-app/src/main/java/study/daydayup/wolf/business/union/depoy/app/UnionAppApplication.java
@@ -0,0 +1,19 @@
+package study.daydayup.wolf.business.union.depoy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * study.daydayup.wolf.business.union.depoy.app
+ *
+ * @author Wingle
+ * @since 2020/1/1 5:24 下午
+ **/
+@SpringBootApplication(scanBasePackages = {"study.daydayup.wolf.business.union"})
+@EnableDiscoveryClient
+public class UnionAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UnionAppApplication.class, args);
+    }
+}
