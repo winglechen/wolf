d573e3e84d34dcf8b3d8469b6accadc5eeb7e9cd
思源 <winglechen@gmail.com>
2020-01-09 15:52:49 +0800

    Customer init when session.isLogin

--- business/uc/uc-agent/src/main/java/study/daydayup/wolf/business/uc/agent/config/UcAutoConfiguration.java
+++ business/uc/uc-agent/src/main/java/study/daydayup/wolf/business/uc/agent/config/UcAutoConfiguration.java
@@ -2,8 +2,11 @@ package study.daydayup.wolf.business.uc.agent.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
+import study.daydayup.wolf.business.account.auth.agent.Session;
 import study.daydayup.wolf.business.uc.agent.setting.AccountStatusAgent;
 import study.daydayup.wolf.business.uc.agent.setting.CustomerStatusAgent;
+
+import javax.annotation.Resource;
 
 
 /**
@@ -15,11 +18,19 @@ import study.daydayup.wolf.business.uc.agent.setting.CustomerStatusAgent;
 @Configuration
 @ComponentScan("study.daydayup.wolf.business.uc.agent")
 public class UcAutoConfiguration {
+    @Resource
+    private Session session;
 
     @Bean
     @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public CustomerStatusAgent customerStatusAgent() {
         CustomerStatusAgent agent = new CustomerStatusAgent();
+
+        if (session.isLogin()) {
+            Long accountId = session.get("accountId", Long.class);
+            Long orgId = session.get("orgId", Long.class);
+            agent.init(accountId, orgId);
+        }
 
         return agent;
     }
@@ -28,6 +39,11 @@ public class UcAutoConfiguration {
     @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public AccountStatusAgent accountStatusAgent() {
         AccountStatusAgent agent = new AccountStatusAgent();
+
+        Long accountId = session.get("accountId", Long.class, false);
+        if (null != accountId) {
+            agent.init(accountId);
+        }
 
         return agent;
     }
