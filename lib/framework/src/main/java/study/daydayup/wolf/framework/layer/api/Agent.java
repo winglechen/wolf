c2281e881aba0c932d82ad9e23657d7473040825
思源 <winglechen@gmail.com>
2020-01-09 15:00:57 +0800

    add Agent interface

---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/layer/api/Agent.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.framework.layer.api;
+
+/**
+ * study.daydayup.wolf.framework.layer.api
+ *
+ * @author Wingle
+ * @since 2020/1/9 1:19 下午
+ **/
+public interface Agent {
+}
