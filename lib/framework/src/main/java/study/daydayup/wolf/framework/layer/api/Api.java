3e7cc746daa441971461af2f8074bc62c002b1a1
思源 <winglechen@gmail.com>
2020-01-11 14:18:41 +0800

    add api and epi interfaces

---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/layer/api/Api.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.framework.layer.api;
+
+import study.daydayup.wolf.framework.layer.domain.Service;
+
+/**
+ * study.daydayup.wolf.framework.layer.api
+ *
+ * @author Wingle
+ * @since 2020/1/11 2:12 下午
+ **/
+public interface Api extends Service {
+}
---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/layer/epi/Epi.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.framework.layer.epi;
+
+import study.daydayup.wolf.framework.layer.domain.Service;
+
+/**
+ * study.daydayup.wolf.framework.layer.epi
+ *
+ * @author Wingle
+ * @since 2020/1/11 2:11 下午
+ **/
+public interface Epi extends Service {
+}
