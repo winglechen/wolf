27bfc37d30f1f45a75849969701ac95b6b6fcd18
思源 <winglechen@gmail.com>
2020-01-14 23:13:56 +0800

    remove test code for CollectionUtilTest

--- lib/common/src/test/java/study/daydayup/wolf/common/util/CollectionUtilTest.java
+++ lib/common/src/test/java/study/daydayup/wolf/common/util/CollectionUtilTest.java
@@ -230,59 +230,6 @@ public class CollectionUtilTest {
         }
     }
 
-//        public static void main(String[] args) {
-//        List<Person> p1 = new ArrayList<>();
-//        List<Person> p2 = new ArrayList<>();
-//        Map<Integer, String> nameMap = new HashMap<>();
-//
-//        Person t1, t2;
-//        for (int i=1; i<6; i++) {
-//            t1 = new Person();
-//            t1.setId(i);
-//
-//            t2 = new Person();
-//            t2.setId(i);
-//            t2.setName("s2_" + i);
-//
-//            if (i % 2 == 0) {
-//                t1.setGender(0);
-//                t2.setGender(0);
-//            } else {
-//                t1.setGender(1);
-//                t2.setGender(1);
-//            }
-//
-//            p1.add(t1);
-//            p2.add(t2);
-//            nameMap.put(i, "name_" + i);
-//        }
-//
-//        List<Integer> ids = CollectionUtil.keys(p1, Person::getId);
-//
-//        System.out.println("persons: " + p1);
-//        System.out.println("ids: " + ids);
-//
-//        Map<Integer, Person> map = toMap(p1, Person::getId);
-//        System.out.println("map: " + map);
-////
-////        join(p1, Person::getId, Person::setName, nameMap);
-////        System.out.println("after join persons: " + p1);
-//        System.out.println("before join: " + p1);
-//        join(p1, Person::getId, Person::setP, p2, Person::getId);
-//        System.out.println("after join: " + p1);
-//
-//
-//        Map<Integer, List<Person>> newMap = group(p1, Person::getGender);
-//        System.out.println("gender Map: " + newMap);
-//
-//
-//        long sum = sum(p1, Person::getId);
-//        System.out.println("sum: " + sum);
-//
-//        Map<Integer, Long> sumMap = groupAndSum(p1, Person::getGender, Person::getId);
-//        System.out.println("sumMap: " + sumMap);
-//    }
-
     @Data
     static class Person {
         private int id;
