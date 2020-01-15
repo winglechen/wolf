c1c90ff425642861d8e68b6883ddd0719bece5fe
思源 <winglechen@gmail.com>
2020-01-15 13:51:20 +0800

    add goods dtos

---
+++ business/goods/goods-api/src/main/java/study/daydayup/wolf/business/goods/api/dto/loan/InstallmentDTO.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.goods.api.dto.loan;
+
+/**
+ * study.daydayup.wolf.business.goods.api.dto.loan
+ *
+ * @author Wingle
+ * @since 2020/1/15 10:14 上午
+ **/
+public class InstallmentDTO {
+    // 0.0000%
+    private Double percentage;
+    private Double feePercentage;
+}
