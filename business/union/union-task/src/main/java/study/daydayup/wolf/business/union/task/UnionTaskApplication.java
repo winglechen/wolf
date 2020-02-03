ac39bccdfbbf88760bf05673a90858c4f33d7134
思源 <winglechen@gmail.com>
2020-02-03 20:27:59 +0800

    add hello controller to union-task

--- business/union/union-task/src/main/java/study/daydayup/wolf/business/union/task/UnionTaskApplication.java
+++ business/union/union-task/src/main/java/study/daydayup/wolf/business/union/task/UnionTaskApplication.java
@@ -1,5 +1,6 @@
 package study.daydayup.wolf.business.union.task;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2020/2/3 7:43 下午
  **/
 @SpringBootApplication
+@MapperScan("study.daydayup.wolf.business.union.task.dal.dao")
 public class UnionTaskApplication {
     public static void main(String[] args) {
         SpringApplication.run(UnionTaskApplication.class, args);
