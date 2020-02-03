bf9f265310e0a6254ff9f07c5c6568a0eab48af0
思源 <winglechen@gmail.com>
2020-02-03 19:47:59 +0800

    add union-task module

---
+++ business/union/union-task/src/main/java/study/daydayup/wolf/business/union/task/UnionTaskApplication.java
@@ -0,0 +1,17 @@
+package study.daydayup.wolf.business.union.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * study.daydayup.wolf.business.union.task
+ *
+ * @author Wingle
+ * @since 2020/2/3 7:43 下午
+ **/
+@SpringBootApplication
+public class UnionTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UnionTaskApplication.class, args);
+    }
+}
