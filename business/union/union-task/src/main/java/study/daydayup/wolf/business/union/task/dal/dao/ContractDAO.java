c466e63e24337ff2c3d9f3c60fb935b54e978d2e
思源 <winglechen@gmail.com>
2020-02-04 18:16:06 +0800

    add jdbcTemplate select demo

---
+++ business/union/union-task/src/main/java/study/daydayup/wolf/business/union/task/dal/dao/ContractDAO.java
@@ -0,0 +1,21 @@
+package study.daydayup.wolf.business.union.task.dal.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import javax.validation.constraints.Max;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * study.daydayup.wolf.business.union.task.dal.dao
+ *
+ * @author Wingle
+ * @since 2020/2/4 12:46 上午
+ **/
+@Mapper
+public interface ContractDAO {
+    @Select("select * from `contract` order by id desc limit 3")
+    List<Map<String, Object>> top();
+}
