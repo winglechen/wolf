cc63f2054afd4dbb788176f9b153a531c8bad9bf
思源 <winglechen@gmail.com>
2020-02-08 18:13:26 +0800

    add empty DailKoiController

---
+++ business/union/union-task/src/main/java/study/daydayup/wolf/business/union/task/controller/DailyKoiController.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.union.task.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * study.daydayup.wolf.business.union.task.controller
+ *
+ * @author Wingle
+ * @since 2020/2/7 4:34 下午
+ **/
+@RestController
+public class DailyKoiController {
+}
---
+++ business/union/union-task/src/main/java/study/daydayup/wolf/business/union/task/controller/DailyLoanController.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.union.task.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * study.daydayup.wolf.business.union.task.controller
+ *
+ * @author Wingle
+ * @since 2020/2/7 4:35 下午
+ **/
+@RestController
+public class DailyLoanController {
+}
