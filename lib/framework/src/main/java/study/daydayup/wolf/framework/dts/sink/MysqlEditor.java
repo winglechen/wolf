7fd049d75995a3f4f4fa3a6682593c3484a8c507
思源 <winglechen@gmail.com>
2020-02-08 18:14:16 +0800

    add MysqlEditor using JdbcTemplate

---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/dts/sink/MysqlEditor.java
@@ -0,0 +1,24 @@
+package study.daydayup.wolf.framework.dts.sink;
+
+import lombok.NonNull;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * study.daydayup.wolf.framework.dts.sink
+ *
+ * @author Wingle
+ * @since 2020/2/8 6:07 下午
+ **/
+@Component
+public class MysqlEditor implements Sink {
+    @Resource
+    private JdbcTemplate jdbc;
+
+    public int update(@NonNull String table, @NonNull Map<String, Object> key, @NonNull Map<String, Object> value) {
+        return 0;
+    }
+}
