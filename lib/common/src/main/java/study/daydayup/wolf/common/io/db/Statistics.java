7b7253eee7ba0c8291bcfb2e73f2f6685150a057
思源 <winglechen@gmail.com>
2020-02-11 00:42:11 +0800

    add Statistics

--- lib/common/src/main/java/study/daydayup/wolf/common/io/db/Statistics.java
+++ lib/common/src/main/java/study/daydayup/wolf/common/io/db/Statistics.java
@@ -1,9 +1,10 @@
 package study.daydayup.wolf.common.io.db;
 
 import lombok.Data;
+import lombok.NonNull;
+import study.daydayup.wolf.common.util.collection.CollectionUtil;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * study.daydayup.wolf.business.union.task.dts
@@ -13,13 +14,11 @@ import java.util.Map;
  **/
 @Data
 public class Statistics {
-    private String taskName;
-    private String tableName;
-    private String shardingKey;
-    private Long offset;
+    private static final String KEY_DELIMITER = ":";
 
     private List<String> keyColumns;
 
+    private Row currentData;
     /**
      * key column map
      * {
@@ -27,6 +26,90 @@ public class Statistics {
      *     columnB : columnB.value
      * }
      */
-    private Map<String, Object> key;
-    private Map<String, Object> value;
+    private Map<String, Row> data;
+
+    public Statistics() {
+        keyColumns = new ArrayList<>();
+        data = new HashMap<>();
+    }
+
+    public void addKeyColumn(String... columns) {
+        if (columns == null || 0 == columns.length) {
+            return;
+        }
+
+        keyColumns.addAll(Arrays.asList(columns));
+    }
+
+    public String addRow(@NonNull Row row) {
+        String key = getKeyFromRow(row);
+        if (key == null) {
+            return null;
+        }
+
+        Row data = getCurrentRow(key);
+        setKeyColumns(data, row);
+
+        return key;
+    }
+
+    public Collection<Row> getRows() {
+        return data.values();
+    }
+
+    public void set(@NonNull String key,@NonNull Object value) {
+        if (null == currentData) {
+            throw new IllegalArgumentException("Please addRow first!");
+        }
+        currentData.put(key, value);
+    }
+
+    public boolean setCurrentKey(@NonNull String key) {
+        Row row = data.get(key);
+        if (row == null) {
+            return false;
+        }
+
+        currentData = row;
+        return true;
+    }
+
+    private String getKeyFromRow(@NonNull Row row) {
+        if (!CollectionUtil.hasValue(keyColumns)) {
+            return null;
+        }
+
+        List<Object> keyList = new ArrayList<>();
+        for (String column : keyColumns) {
+            Object value = row.get(column);
+            if (value == null) {
+                return null;
+            }
+            keyList.add(value);
+        }
+
+        return CollectionUtil.join(KEY_DELIMITER, keyList);
+    }
+
+    private Row getCurrentRow(@NonNull String key) {
+        Row row = data.get(key);
+        if (row == null) {
+            row = new Row();
+            data.put(key, row);
+        }
+
+        currentData = row;
+        return row;
+    }
+
+    private void setKeyColumns(Row data, Row row) {
+        if (!CollectionUtil.hasValue(keyColumns)) {
+            return;
+        }
+
+        for (String column : keyColumns) {
+            data.put(column, row.get(column));
+        }
+    }
+
 }
