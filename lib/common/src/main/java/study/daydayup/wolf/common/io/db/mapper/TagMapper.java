03bd635295567ce8e6c5f232c549691cde748e44
思源 <winglechen@gmail.com>
2020-02-11 18:10:33 +0800

    add empty mapper

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/io/db/mapper/TagMapper.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.common.io.db.mapper;
+
+/**
+ * study.daydayup.wolf.common.io.db.mapper
+ *
+ * @author Wingle
+ * @since 2020/2/11 6:09 下午
+ **/
+public class TagMapper extends AbstractMapper implements Mapper {
+}
