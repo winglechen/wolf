e7ca22b09ba2d7bf3d1d9baf8cd99cc816655f3b
思源 <winglechen@gmail.com>
2020-02-16 18:44:02 +0800

    add dts config package

---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/dts/config/SinkConfig.java
@@ -0,0 +1,14 @@
+package study.daydayup.wolf.framework.dts.config;
+
+import lombok.Data;
+
+/**
+ * study.daydayup.wolf.business.union.task.config
+ *
+ * @author Wingle
+ * @since 2020/2/16 4:06 下午
+ **/
+@Data
+public class SinkConfig {
+    private String sinkName;
+}
