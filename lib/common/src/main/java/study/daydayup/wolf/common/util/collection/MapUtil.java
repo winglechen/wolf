d50ac24f7acc57b7b8b43c681da34589182fd60e
思源 <winglechen@gmail.com>
2020-02-18 08:20:54 +0800

    add filterNulls to MapUtil.hasValue

--- lib/common/src/main/java/study/daydayup/wolf/common/util/collection/MapUtil.java
+++ lib/common/src/main/java/study/daydayup/wolf/common/util/collection/MapUtil.java
@@ -14,7 +14,33 @@ public class MapUtil {
     }
 
     public static <K, V> boolean hasValue(Map<K, V> map) {
-        return null != map && 0 != map.size();
+        return hasValue(map, false);
+    }
+
+    public static <K, V> boolean hasValue(Map<K, V> map, boolean filterNulls) {
+        if (map == null) {
+            return false;
+        }
+
+        if (map.isEmpty()) {
+            return false;
+        }
+
+        if (!filterNulls) {
+            return true;
+        }
+
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (null != entry.getKey() && null != entry.getValue()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static <K, V> boolean isEmpty(Map<K, V> map, boolean filterNulls) {
+        return ! hasValue(map, filterNulls);
     }
 
     public static <K, V> boolean isEmpty(Map<K, V> map) {
@@ -67,4 +93,22 @@ public class MapUtil {
     public static <K, V> Map<K, V> subMap(Map<K, V> map, K... keys) {
         return subMap(map, Arrays.asList(keys));
     }
+
+    public static <K, V> Map<K, V> remove(Map<K, V> map, Collection<K> keys) {
+        if (CollectionUtil.isEmpty(keys)) {
+            return map;
+        }
+
+        for (K k : keys) {
+            map.remove(k);
+        }
+
+        return map;
+    }
+
+    @SafeVarargs
+    public static <K, V> Map<K, V> remove(Map<K, V> map, K... keys) {
+        return remove(map, Arrays.asList(keys));
+    }
+
 }
