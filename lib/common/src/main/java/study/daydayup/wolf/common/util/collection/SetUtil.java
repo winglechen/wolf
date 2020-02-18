f175c4580e1064986a3ac19f81a3ac96bf82d026
思源 <winglechen@gmail.com>
2020-02-18 12:07:06 +0800

    add SetUtil

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/util/collection/SetUtil.java
@@ -0,0 +1,19 @@
+package study.daydayup.wolf.common.util.collection;
+
+import java.util.Set;
+
+/**
+ * study.daydayup.wolf.common.util.collection
+ *
+ * @author Wingle
+ * @since 2020/2/18 10:36 上午
+ **/
+public class SetUtil {
+    public static <E> boolean isEmpty(Set<E> set) {
+        return null == set || set.isEmpty();
+    }
+
+    public static <E> boolean hasValue(Set<E> set) {
+        return ! isEmpty(set);
+    }
+}
