2b8455bfab6af496ec8dcab9c84d114d9b1a5a83
思源 <winglechen@gmail.com>
2020-02-20 16:48:04 +0800

    add CodeGroupEnum

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/lang/enums/CodeGroupEnum.java
@@ -0,0 +1,11 @@
+package study.daydayup.wolf.common.lang.enums;
+
+/**
+ * study.daydayup.wolf.common.lang.enums
+ *
+ * @author Wingle
+ * @since 2020/2/20 12:18 下午
+ **/
+public interface CodeGroupEnum extends CodeBasedEnum {
+    int getGroup();
+}
