7e02e009c4c0b1019b28b80a0429b7197738e176
思源 <winglechen@gmail.com>
2020-02-21 00:38:44 +0800

    CustomerStatus remove session dependency

--- business/uc/uc-agent/src/main/java/study/daydayup/wolf/business/uc/agent/config/UcAutoConfiguration.java
+++ business/uc/uc-agent/src/main/java/study/daydayup/wolf/business/uc/agent/config/UcAutoConfiguration.java
@@ -2,11 +2,8 @@ package study.daydayup.wolf.business.uc.agent.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
-import study.daydayup.wolf.business.account.auth.agent.Session;
 import study.daydayup.wolf.business.uc.agent.setting.AccountStatusAgent;
 import study.daydayup.wolf.business.uc.agent.setting.CustomerStatusAgent;
-
-import javax.annotation.Resource;
 
 
 /**
@@ -18,34 +15,17 @@ import javax.annotation.Resource;
 @Configuration
 @ComponentScan("study.daydayup.wolf.business.uc.agent")
 public class UcAutoConfiguration {
-    @Resource
-    private Session session;
 
     @Bean
     @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public CustomerStatusAgent customerStatusAgent() {
-        CustomerStatusAgent agent = new CustomerStatusAgent();
-
-        if (session.isLogin()) {
-            Long accountId = session.get("accountId", Long.class);
-            Long orgId = session.get("orgId", Long.class);
-            agent.init(accountId, orgId);
-        }
-
-        return agent;
+        return new CustomerStatusAgent();
     }
 
     @Bean
     @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
     public AccountStatusAgent accountStatusAgent() {
-        AccountStatusAgent agent = new AccountStatusAgent();
-
-        Long accountId = session.get("accountId", Long.class, false);
-        if (null != accountId) {
-            agent.init(accountId);
-        }
-
-        return agent;
+        return new AccountStatusAgent();
     }
 
 
