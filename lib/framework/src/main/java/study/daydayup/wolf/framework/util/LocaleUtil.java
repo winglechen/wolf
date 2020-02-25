0d993acf2527d27fa75de01c9974a4e527c0e0a7
思源 <winglechen@gmail.com>
2020-02-25 16:04:50 +0800

    remove useless code

--- lib/framework/src/main/java/study/daydayup/wolf/framework/util/LocaleUtil.java
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/util/LocaleUtil.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
-import study.daydayup.wolf.common.util.collection.ArrayUtil;
 import study.daydayup.wolf.common.util.lang.StringUtil;
 
 import java.util.Locale;
