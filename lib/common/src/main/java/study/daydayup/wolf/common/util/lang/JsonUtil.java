14ce0932366cc0cbecf399773436ede4697c23b9
思源 <winglechen@gmail.com>
2020-02-26 13:07:41 +0800

    add JsonUtil for FastJson

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/util/lang/JsonUtil.java
@@ -0,0 +1,36 @@
+package study.daydayup.wolf.common.util.lang;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.NonNull;
+import study.daydayup.wolf.common.util.collection.ArrayUtil;
+
+/**
+ * study.daydayup.wolf.common.util.lang
+ *
+ * @author Wingle
+ * @since 2020/2/26 1:00 下午
+ **/
+public class JsonUtil {
+    private static final String KEY_SEPARATOR = ".";
+
+    public static JSONObject getJSONObject(@NonNull JSONObject obj, @NonNull String keyString) {
+        String[] keys = StringUtil.split(keyString, KEY_SEPARATOR);
+        return getJSONObject(obj, keys);
+    }
+
+    public static JSONObject getJSONObject(@NonNull JSONObject obj, String... keys) {
+        if (ArrayUtil.isEmpty(keys)) {
+            return null;
+        }
+
+        JSONObject tmp = obj;
+        for (String key : keys) {
+            tmp = tmp.getJSONObject(key);
+            if (0 == tmp.size()) {
+                return null;
+            }
+        }
+
+        return tmp;
+    }
+}
