403b9a8e1e561f3c4e3594a2488f259d9f6ff8ca
思源 <winglechen@gmail.com>
2020-02-27 19:52:22 +0800

    add empty RazorpayServiceImpl

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/api/china/RazorpayServiceImpl.java
@@ -0,0 +1,31 @@
+package study.daydayup.wolf.business.pay.biz.api.china;
+
+import study.daydayup.wolf.business.pay.api.dto.PayRequest;
+import study.daydayup.wolf.business.pay.api.dto.PayResponse;
+import study.daydayup.wolf.business.pay.api.dto.PaymentCreateRequest;
+import study.daydayup.wolf.business.pay.api.dto.PaymentCreateResponse;
+import study.daydayup.wolf.business.pay.api.service.india.RazorpayService;
+import study.daydayup.wolf.framework.rpc.Result;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.api.china
+ *
+ * @author Wingle
+ * @since 2020/2/27 7:37 下午
+ **/
+public class RazorpayServiceImpl implements RazorpayService {
+    @Override
+    public Result<PaymentCreateResponse> create(PaymentCreateRequest request) {
+        return null;
+    }
+
+    @Override
+    public Result<PayResponse> pay(PayRequest request) {
+        return null;
+    }
+
+    @Override
+    public Result<String> subscribe(String data) {
+        return null;
+    }
+}
---
+++ business/union/union-app/src/main/java/study/daydayup/wolf/business/union/app/service/RazorpayWebService.java
@@ -0,0 +1,39 @@
+package study.daydayup.wolf.business.union.app.service;
+
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.pay.api.dto.PaymentCreateRequest;
+import study.daydayup.wolf.business.pay.api.dto.PaymentCreateResponse;
+import study.daydayup.wolf.business.pay.api.service.india.RazorpayService;
+import study.daydayup.wolf.business.trade.api.service.buy.LoanService;
+import study.daydayup.wolf.framework.rpc.Result;
+
+/**
+ * study.daydayup.wolf.business.union.app.service
+ *
+ * @author Wingle
+ * @since 2020/2/27 7:42 下午
+ **/
+@Component
+public class RazorpayWebService {
+    @Reference
+    private LoanService loanService;
+    @Reference
+    private RazorpayService razorpayService;
+
+    public Result<PaymentCreateResponse> create(PaymentCreateRequest request) {
+        //获取合同信息
+
+        //确认分期情况
+
+        //查找分期还款订单
+            //存在则查询还款订单
+            //不存在则生成还款订单
+
+        //格式化支付参数
+
+        //支付请求
+
+        return null;
+    }
+}
