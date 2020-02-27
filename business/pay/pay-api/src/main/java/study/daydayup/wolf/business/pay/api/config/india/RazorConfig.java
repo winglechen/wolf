a3506cd9f119b727016a5898dfa3e9f45ba0ecf0
思源 <winglechen@gmail.com>
2020-02-27 16:19:08 +0800

    add pay config packages

---
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/config/india/RazorConfig.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.pay.api.config.india;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * study.daydayup.wolf.business.pay.api.config.india
+ *
+ * @author Wingle
+ * @since 2020/2/27 4:18 下午
+ **/
+@Configuration
+public class RazorConfig {
+}
--- business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/dto/PaymentCreateResponse.java
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/dto/PaymentCreateResponse.java
@@ -1,10 +1,23 @@
 package study.daydayup.wolf.business.pay.api.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
 /**
  * study.daydayup.wolf.business.pay.api.dto
  *
  * @author Wingle
  * @since 2020/2/27 3:54 下午
  **/
+@Data
 public class PaymentCreateResponse {
+    private Integer paymentMethod;
+
+    private String paymentNo;
+    private BigDecimal amount;
+    private Long longAmount;
+
+    private Map<String, Object> attachment;
 }
