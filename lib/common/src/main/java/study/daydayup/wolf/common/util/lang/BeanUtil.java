051cadd4afebde231e7778fd148c99b604f70da0
思源 <winglechen@gmail.com>
2020-02-27 10:35:09 +0800

    BeanUtil add toMap and toBean method

--- lib/common/src/main/java/study/daydayup/wolf/common/util/lang/BeanUtil.java
+++ lib/common/src/main/java/study/daydayup/wolf/common/util/lang/BeanUtil.java
@@ -1,5 +1,13 @@
 package study.daydayup.wolf.common.util.lang;
 
+import lombok.NonNull;
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * study.daydayup.wolf.common.util.lang
  *
@@ -13,6 +21,48 @@ public class BeanUtil {
         }
 
         return a.equals(b);
+    }
+
+    public static Map<String, Object> toMap(@NonNull Object object) {
+        Map<String, Object> map = new HashMap<>();
+        Field[] fields = object.getClass().getDeclaredFields();
+
+        try {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                map.put(field.getName(), field.get(object));
+            }
+        } catch (IllegalAccessException e) {
+            return null;
+        }
+
+        return map;
+    }
+
+    @SneakyThrows
+    public static <T> T toBean(@NonNull Map<String, Object> map, Class<T> beanClass) {
+        T object;
+        try {
+            object = beanClass.newInstance();
+            Field[] fields = object.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                int mod = field.getModifiers();
+                if (Modifier.isStatic(mod) || Modifier.isFinal(mod)) {
+                    continue;
+                }
+                field.setAccessible(true);
+                if (map.containsKey(field.getName())) {
+                    field.set(object, map.get(field.getName()));
+                }
+            }
+        } catch (InstantiationException e) {
+            //log
+            throw e;
+        } catch (IllegalAccessException e) {
+            return null;
+        }
+
+        return object;
     }
 
 }
