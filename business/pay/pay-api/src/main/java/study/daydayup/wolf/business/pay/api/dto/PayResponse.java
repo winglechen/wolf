bf60d738602b211255fcf8ea917091a897acaa62
思源 <winglechen@gmail.com>
2020-02-27 17:02:48 +0800

    add PayService

--- business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/dto/PayResponse.java
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/dto/PayResponse.java
@@ -2,6 +2,9 @@ package study.daydayup.wolf.business.pay.api.dto;
 
 import study.daydayup.wolf.framework.layer.api.Response;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 
 /**
  * study.daydayup.wolf.business.pay.api.dto
@@ -10,7 +13,8 @@ import study.daydayup.wolf.framework.layer.api.Response;
  * @since 2020/2/27 4:43 下午
  **/
 public class PayResponse extends Attachment implements Response {
-
-
-
+    private Integer paymentMethod;
+    private BigDecimal amount;
+    private Boolean success;
+    private LocalDateTime updateAt;
 }
---
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/service/PayService.java
@@ -0,0 +1,31 @@
+package study.daydayup.wolf.business.pay.api.service;
+
+import study.daydayup.wolf.business.pay.api.dto.PayRequest;
+import study.daydayup.wolf.business.pay.api.dto.PayResponse;
+import study.daydayup.wolf.business.pay.api.dto.PaymentCreateRequest;
+import study.daydayup.wolf.business.pay.api.dto.PaymentCreateResponse;
+import study.daydayup.wolf.framework.rpc.Result;
+
+/**
+ * study.daydayup.wolf.business.pay.api.service
+ *
+ * @author Wingle
+ * @since 2020/2/27 10:40 上午
+ **/
+public interface PayService {
+    Result<PaymentCreateResponse> create(PaymentCreateRequest request);
+
+    /**
+     * pay
+     * 一般是前端(H5, app, ...)发起
+     */
+    Result<PayResponse> pay(PayRequest request);
+
+    /**
+     * 订阅异步通知
+     * @param data
+     * @return
+     */
+    Result<String> subscribe(String data);
+
+}
