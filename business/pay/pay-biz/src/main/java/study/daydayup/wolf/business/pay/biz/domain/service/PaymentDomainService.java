fd8065c8012bfea340a01d50a8bae6f8187728e7
思源 <winglechen@gmail.com>
2020-03-01 01:16:48 +0800

    add PaymentDomainService

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/domain/service/PaymentDomainService.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.business.pay.biz.domain.service;
+
+import study.daydayup.wolf.framework.layer.domain.Service;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.domain.service
+ *
+ * @author Wingle
+ * @since 2020/3/1 12:45 上午
+ **/
+public interface PaymentDomainService extends Service {
+}
---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/domain/service/impl/PaymentDomainServiceImpl.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.business.pay.biz.domain.service.impl;
+
+import study.daydayup.wolf.business.pay.biz.domain.service.PaymentDomainService;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.domain.service.impl
+ *
+ * @author Wingle
+ * @since 2020/3/1 1:00 上午
+ **/
+public class PaymentDomainServiceImpl implements PaymentDomainService {
+}
