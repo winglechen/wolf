93e03d0701605405e2e3a89e0144b3400ecb0c07
思源 <winglechen@gmail.com>
2020-03-01 12:24:08 +0800

    regenerator Installment sql files

--- business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/biz/dal/dataobject/InstallmentTermDO.java
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/biz/dal/dataobject/InstallmentTermDO.java
@@ -12,6 +12,8 @@ public class InstallmentTermDO implements Serializable {
 
     private String tradeNo;
 
+    private String relatedTradeNo;
+
     private Long buyerId;
 
     private Long sellerId;
@@ -22,29 +24,25 @@ public class InstallmentTermDO implements Serializable {
 
     private Integer state;
 
-    private String relatedTradeNo;
-
     private LocalDate effectAt;
 
     private LocalDate dueAt;
 
-    private Long amount;
-
     private Integer currency;
 
-    private Long paidAmount;
+    private BigDecimal amount;
 
-    private Long lossAmount;
+    private BigDecimal paidAmount;
+
+    private BigDecimal lossAmount;
 
     private BigDecimal interest;
+
+    private BigDecimal penalty;
 
     private BigDecimal handlingFee;
 
     private Integer period;
-
-    private Integer periodUnit;
-
-    private Integer periodStrategy;
 
     private BigDecimal percentage;
 
@@ -70,23 +68,22 @@ public class InstallmentTermDO implements Serializable {
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
         sb.append(", tradeNo=").append(tradeNo);
+        sb.append(", relatedTradeNo=").append(relatedTradeNo);
         sb.append(", buyerId=").append(buyerId);
         sb.append(", sellerId=").append(sellerId);
         sb.append(", installmentNo=").append(installmentNo);
         sb.append(", installmentType=").append(installmentType);
         sb.append(", state=").append(state);
-        sb.append(", relatedTradeNo=").append(relatedTradeNo);
         sb.append(", effectAt=").append(effectAt);
         sb.append(", dueAt=").append(dueAt);
-        sb.append(", amount=").append(amount);
         sb.append(", currency=").append(currency);
+        sb.append(", amount=").append(amount);
         sb.append(", paidAmount=").append(paidAmount);
         sb.append(", lossAmount=").append(lossAmount);
         sb.append(", interest=").append(interest);
+        sb.append(", penalty=").append(penalty);
         sb.append(", handlingFee=").append(handlingFee);
         sb.append(", period=").append(period);
-        sb.append(", periodUnit=").append(periodUnit);
-        sb.append(", periodStrategy=").append(periodStrategy);
         sb.append(", percentage=").append(percentage);
         sb.append(", feePercentage=").append(feePercentage);
         sb.append(", version=").append(version);
@@ -112,23 +109,22 @@ public class InstallmentTermDO implements Serializable {
         InstallmentTermDO other = (InstallmentTermDO) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
             && (this.getTradeNo() == null ? other.getTradeNo() == null : this.getTradeNo().equals(other.getTradeNo()))
+            && (this.getRelatedTradeNo() == null ? other.getRelatedTradeNo() == null : this.getRelatedTradeNo().equals(other.getRelatedTradeNo()))
             && (this.getBuyerId() == null ? other.getBuyerId() == null : this.getBuyerId().equals(other.getBuyerId()))
             && (this.getSellerId() == null ? other.getSellerId() == null : this.getSellerId().equals(other.getSellerId()))
             && (this.getInstallmentNo() == null ? other.getInstallmentNo() == null : this.getInstallmentNo().equals(other.getInstallmentNo()))
             && (this.getInstallmentType() == null ? other.getInstallmentType() == null : this.getInstallmentType().equals(other.getInstallmentType()))
             && (this.getState() == null ? other.getState() == null : this.getState().equals(other.getState()))
-            && (this.getRelatedTradeNo() == null ? other.getRelatedTradeNo() == null : this.getRelatedTradeNo().equals(other.getRelatedTradeNo()))
             && (this.getEffectAt() == null ? other.getEffectAt() == null : this.getEffectAt().equals(other.getEffectAt()))
             && (this.getDueAt() == null ? other.getDueAt() == null : this.getDueAt().equals(other.getDueAt()))
-            && (this.getAmount() == null ? other.getAmount() == null : this.getAmount().equals(other.getAmount()))
             && (this.getCurrency() == null ? other.getCurrency() == null : this.getCurrency().equals(other.getCurrency()))
+            && (this.getAmount() == null ? other.getAmount() == null : this.getAmount().equals(other.getAmount()))
             && (this.getPaidAmount() == null ? other.getPaidAmount() == null : this.getPaidAmount().equals(other.getPaidAmount()))
             && (this.getLossAmount() == null ? other.getLossAmount() == null : this.getLossAmount().equals(other.getLossAmount()))
             && (this.getInterest() == null ? other.getInterest() == null : this.getInterest().equals(other.getInterest()))
+            && (this.getPenalty() == null ? other.getPenalty() == null : this.getPenalty().equals(other.getPenalty()))
             && (this.getHandlingFee() == null ? other.getHandlingFee() == null : this.getHandlingFee().equals(other.getHandlingFee()))
             && (this.getPeriod() == null ? other.getPeriod() == null : this.getPeriod().equals(other.getPeriod()))
-            && (this.getPeriodUnit() == null ? other.getPeriodUnit() == null : this.getPeriodUnit().equals(other.getPeriodUnit()))
-            && (this.getPeriodStrategy() == null ? other.getPeriodStrategy() == null : this.getPeriodStrategy().equals(other.getPeriodStrategy()))
             && (this.getPercentage() == null ? other.getPercentage() == null : this.getPercentage().equals(other.getPercentage()))
             && (this.getFeePercentage() == null ? other.getFeePercentage() == null : this.getFeePercentage().equals(other.getFeePercentage()))
             && (this.getVersion() == null ? other.getVersion() == null : this.getVersion().equals(other.getVersion()))
@@ -144,23 +140,22 @@ public class InstallmentTermDO implements Serializable {
         int result = 1;
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
         result = prime * result + ((getTradeNo() == null) ? 0 : getTradeNo().hashCode());
+        result = prime * result + ((getRelatedTradeNo() == null) ? 0 : getRelatedTradeNo().hashCode());
         result = prime * result + ((getBuyerId() == null) ? 0 : getBuyerId().hashCode());
         result = prime * result + ((getSellerId() == null) ? 0 : getSellerId().hashCode());
         result = prime * result + ((getInstallmentNo() == null) ? 0 : getInstallmentNo().hashCode());
         result = prime * result + ((getInstallmentType() == null) ? 0 : getInstallmentType().hashCode());
         result = prime * result + ((getState() == null) ? 0 : getState().hashCode());
-        result = prime * result + ((getRelatedTradeNo() == null) ? 0 : getRelatedTradeNo().hashCode());
         result = prime * result + ((getEffectAt() == null) ? 0 : getEffectAt().hashCode());
         result = prime * result + ((getDueAt() == null) ? 0 : getDueAt().hashCode());
-        result = prime * result + ((getAmount() == null) ? 0 : getAmount().hashCode());
         result = prime * result + ((getCurrency() == null) ? 0 : getCurrency().hashCode());
+        result = prime * result + ((getAmount() == null) ? 0 : getAmount().hashCode());
         result = prime * result + ((getPaidAmount() == null) ? 0 : getPaidAmount().hashCode());
         result = prime * result + ((getLossAmount() == null) ? 0 : getLossAmount().hashCode());
         result = prime * result + ((getInterest() == null) ? 0 : getInterest().hashCode());
+        result = prime * result + ((getPenalty() == null) ? 0 : getPenalty().hashCode());
         result = prime * result + ((getHandlingFee() == null) ? 0 : getHandlingFee().hashCode());
         result = prime * result + ((getPeriod() == null) ? 0 : getPeriod().hashCode());
-        result = prime * result + ((getPeriodUnit() == null) ? 0 : getPeriodUnit().hashCode());
-        result = prime * result + ((getPeriodStrategy() == null) ? 0 : getPeriodStrategy().hashCode());
         result = prime * result + ((getPercentage() == null) ? 0 : getPercentage().hashCode());
         result = prime * result + ((getFeePercentage() == null) ? 0 : getFeePercentage().hashCode());
         result = prime * result + ((getVersion() == null) ? 0 : getVersion().hashCode());
