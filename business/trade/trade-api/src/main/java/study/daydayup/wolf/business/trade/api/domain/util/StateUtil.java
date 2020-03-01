506ae16c1325ca0630ea3e3fe239504d9d5c0674
思源 <winglechen@gmail.com>
2020-03-01 15:41:49 +0800

    add StateUtil

---
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/domain/util/StateUtil.java
@@ -0,0 +1,20 @@
+package study.daydayup.wolf.business.trade.api.domain.util;
+
+import study.daydayup.wolf.business.trade.api.domain.state.TradeState;
+import study.daydayup.wolf.common.util.lang.BeanUtil;
+
+/**
+ * study.daydayup.wolf.business.trade.api.domain.util
+ *
+ * @author Wingle
+ * @since 2020/3/1 3:24 下午
+ **/
+public class StateUtil {
+    public static  boolean equals(Integer code, TradeState state) {
+        return BeanUtil.equals(code, state.getCode());
+    }
+
+    public static  boolean equals(TradeState state1, TradeState state2) {
+        return BeanUtil.equals(state1.getCode(), state2.getCode());
+    }
+}
