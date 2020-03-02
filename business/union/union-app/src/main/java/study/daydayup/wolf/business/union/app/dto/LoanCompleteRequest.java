90ea3cd42c6c240d7149b607180060a82a4891c6
思源 <winglechen@gmail.com>
2020-03-02 19:05:28 +0800

    add dto package for union-app

---
+++ business/union/union-app/src/main/java/study/daydayup/wolf/business/union/app/dto/LoanCompleteRequest.java
@@ -0,0 +1,14 @@
+package study.daydayup.wolf.business.union.app.dto;
+
+import lombok.Data;
+import study.daydayup.wolf.framework.layer.api.Request;
+
+/**
+ * study.daydayup.wolf.business.union.app.dto
+ *
+ * @author Wingle
+ * @since 2020/3/2 6:56 下午
+ **/
+@Data
+public class LoanCompleteRequest implements Request {
+}
