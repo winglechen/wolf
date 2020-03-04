2bc7a0f71636050f5f5947cb3fdba68dfb5a72ae
思源 <winglechen@gmail.com>
2020-03-04 10:32:32 +0800

    add empty crm feed and profile module

---
+++ business/uc/crm-biz/src/main/java/study/daydayup/wolf/business/uc/crm/biz/CrmApplication.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.uc.crm.biz;
+
+/**
+ * study.daydayup.wolf.business.uc.crm.biz
+ *
+ * @author Wingle
+ * @since 2020/3/4 10:24 上午
+ **/
+public class CrmApplication {
+}
---
+++ business/uc/feed-biz/src/main/java/study/daydayup/wolf/business/uc/feed/biz/FeedApplication.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.uc.feed.biz;
+
+/**
+ * study.daydayup.wolf.business.uc.feed.biz
+ *
+ * @author Wingle
+ * @since 2020/3/4 10:25 上午
+ **/
+public class FeedApplication {
+}
