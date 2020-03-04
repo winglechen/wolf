e3089bfdc3f39935097d8e0891c6d2808f414c54
思源 <winglechen@gmail.com>
2020-03-04 12:50:19 +0800

    add PayResultResponse

--- business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/dto/buy/base/response/PayResultResponse.java
+++ business/trade/trade-api/src/main/java/study/daydayup/wolf/business/trade/api/dto/buy/base/response/PayResultResponse.java
@@ -2,6 +2,8 @@ package study.daydayup.wolf.business.trade.api.dto.buy.base.response;
 
 import study.daydayup.wolf.framework.layer.api.Response;
 
+import java.math.BigDecimal;
+
 /**
  * study.daydayup.wolf.business.trade.api.dto.buy.base.response
  *
@@ -9,4 +11,7 @@ import study.daydayup.wolf.framework.layer.api.Response;
  * @since 2019/10/9 2:08 下午
  **/
 public class PayResultResponse implements Response {
+    private String tradeNo;
+    private Boolean success;
+    private BigDecimal amount;
 }
