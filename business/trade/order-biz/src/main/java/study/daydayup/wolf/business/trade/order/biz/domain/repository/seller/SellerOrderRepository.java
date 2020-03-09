5ec529f9b7dd9bf5986d0023f564e8f7c7e35953
思源 <winglechen@gmail.com>
2020-03-09 17:20:28 +0800

    add SellerOrderRepository

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/biz/domain/repository/seller/SellerOrderRepository.java
@@ -0,0 +1,101 @@
+package study.daydayup.wolf.business.trade.order.biz.domain.repository.seller;
+
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.trade.api.domain.entity.Contract;
+import study.daydayup.wolf.business.trade.api.domain.entity.Order;
+import study.daydayup.wolf.business.trade.api.dto.TradeOwner;
+import study.daydayup.wolf.business.trade.api.dto.tm.contract.seller.BuyerRequest;
+import study.daydayup.wolf.business.trade.api.dto.tm.contract.seller.StateRequest;
+import study.daydayup.wolf.business.trade.api.dto.tm.contract.seller.TypeRequest;
+import study.daydayup.wolf.business.trade.api.dto.tm.trade.TradeIds;
+import study.daydayup.wolf.business.trade.order.biz.dal.dao.ContractDAO;
+import study.daydayup.wolf.business.trade.order.biz.dal.dao.OrderDAO;
+import study.daydayup.wolf.business.trade.order.biz.dal.dataobject.OrderDO;
+import study.daydayup.wolf.business.trade.order.biz.domain.repository.ContractQueryRepository;
+import study.daydayup.wolf.business.trade.order.biz.domain.repository.OrderQueryRepository;
+import study.daydayup.wolf.common.util.collection.ListUtil;
+import study.daydayup.wolf.framework.rpc.page.Page;
+import study.daydayup.wolf.framework.rpc.page.PageRequest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * study.daydayup.wolf.business.trade.order.biz.domain.repository.seller
+ *
+ * @author Wingle
+ * @since 2020/1/18 10:35 下午
+ **/
+@Component
+public class SellerOrderRepository extends OrderQueryRepository {
+    @Resource
+    private OrderDAO orderDAO;
+
+    public List<Order> findByTradeNos(TradeIds tradeIds) {
+        tradeIds.valid();
+        List<OrderDO> orderDOList = orderDAO.selectByTradeNoIn(tradeIds.getTradeNoSet(), tradeIds.getBuyerId(), tradeIds.getSellerId());
+        if (orderDOList == null || orderDOList.isEmpty()) {
+            return ListUtil.empty();
+        }
+
+        TradeOwner owner = new TradeOwner();
+        owner.setSellerId(tradeIds.getSellerId());
+        owner.setBuyerId(tradeIds.getBuyerId());
+
+        return findExtraByOrderList(orderDOList, owner);
+    }
+
+    public Page<Order> findAll(@NonNull Long sellerId, PageRequest pageReq) {
+        Page.startPage(pageReq.getPageNum(), pageReq.getPageSize());
+        List<OrderDO> orderDOList = orderDAO.selectBySellerId(sellerId);
+        if (orderDOList == null || orderDOList.isEmpty()) {
+            return Page.empty(pageReq.getPageNum(), pageReq.getPageSize());
+        }
+        TradeOwner owner = new TradeOwner();
+        owner.setSellerId(sellerId);
+
+        List<Order> orderList = findExtraByOrderList(orderDOList, owner);
+        return Page.of(orderDOList).to(orderList);
+    }
+
+    public Page<Order> findByTradeType(TypeRequest request, PageRequest pageReq) {
+        Page.startPage(pageReq.getPageNum(), pageReq.getPageSize());
+        List<OrderDO> orderDOList = orderDAO.sellerByTradeType(request);
+        if (orderDOList == null || orderDOList.isEmpty()) {
+            return Page.empty(pageReq.getPageNum(), pageReq.getPageSize());
+        }
+        TradeOwner owner = new TradeOwner();
+        owner.setSellerId(request.getSellerId());
+
+        List<Order> orderList = findExtraByOrderList(orderDOList, owner);
+        return Page.of(orderDOList).to(orderList);
+    }
+
+    public Page<Order> findByTradeState(StateRequest request, PageRequest pageReq) {
+        Page.startPage(pageReq.getPageNum(), pageReq.getPageSize());
+        List<OrderDO> orderDOList = orderDAO.sellerByState(request);
+        if (orderDOList == null || orderDOList.isEmpty()) {
+            return Page.empty(pageReq.getPageNum(), pageReq.getPageSize());
+        }
+        TradeOwner owner = new TradeOwner();
+        owner.setSellerId(request.getSellerId());
+
+        List<Order> orderList = findExtraByOrderList(orderDOList, owner);
+        return Page.of(orderDOList).to(orderList);
+    }
+
+    public Page<Order> findByBuyerId(BuyerRequest request, PageRequest pageReq) {
+        Page.startPage(pageReq.getPageNum(), pageReq.getPageSize());
+        List<OrderDO> orderDOList = orderDAO.sellerByBuyerId(request);
+        if (orderDOList == null || orderDOList.isEmpty()) {
+            return Page.empty(pageReq.getPageNum(), pageReq.getPageSize());
+        }
+        TradeOwner owner = new TradeOwner();
+        owner.setSellerId(request.getSellerId());
+
+        List<Order> orderList = findExtraByOrderList(orderDOList, owner);
+        return Page.of(orderDOList).to(orderList);
+    }
+
+}
