a78a6f94203f1ee4d81ad8011a0a4980b2069b34
思源 <winglechen@gmail.com>
2020-03-10 16:37:15 +0800

    add CreditLogDomainService

---
+++ business/uc/crm-biz/src/main/java/study/daydayup/wolf/business/uc/crm/biz/customer/credit/service/impl/CreditLogDomainServiceImpl.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.business.uc.crm.biz.customer.credit.service.impl;
+
+import study.daydayup.wolf.business.uc.crm.biz.customer.credit.service.CreditLogDomainService;
+
+/**
+ * study.daydayup.wolf.business.uc.crm.biz.customer.credit.service.impl
+ *
+ * @author Wingle
+ * @since 2020/3/10 4:15 下午
+ **/
+public class CreditLogDomainServiceImpl implements CreditLogDomainService {
+}
