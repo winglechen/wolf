e812c24357ddf6095e49fe0e170cd26edb6bb4a1
思源 <winglechen@gmail.com>
2020-03-16 18:23:43 +0800

    impl TaskQueryRepository.findDetailsByTaskList()

--- business/organization/org-biz/src/main/java/study/daydayup/wolf/business/org/biz/task/domain/repository/TaskQueryRepository.java
+++ business/organization/org-biz/src/main/java/study/daydayup/wolf/business/org/biz/task/domain/repository/TaskQueryRepository.java
@@ -3,14 +3,19 @@ package study.daydayup.wolf.business.org.biz.task.domain.repository;
 import lombok.NonNull;
 import org.springframework.stereotype.Component;
 import study.daydayup.wolf.business.org.api.task.domain.entity.Task;
+import study.daydayup.wolf.business.org.api.task.domain.entity.task.TaskContact;
+import study.daydayup.wolf.business.org.api.task.domain.entity.task.TaskTrade;
 import study.daydayup.wolf.business.org.api.task.dto.TaskId;
 import study.daydayup.wolf.business.org.api.task.dto.TaskIds;
 import study.daydayup.wolf.business.org.api.task.dto.TaskOwner;
 import study.daydayup.wolf.business.org.api.task.dto.request.task.ProjectRequest;
 import study.daydayup.wolf.business.org.api.task.dto.request.task.StaffRequest;
 import study.daydayup.wolf.business.org.api.task.dto.request.task.TaskTypeRequest;
+import study.daydayup.wolf.business.org.biz.task.converter.TaskConverter;
 import study.daydayup.wolf.business.org.biz.task.dal.dataobject.TaskDO;
-import study.daydayup.wolf.business.org.biz.task.domain.entity.TaskEntity;
+import study.daydayup.wolf.business.org.biz.task.dal.dataobject.TaskDetailDO;
+import study.daydayup.wolf.common.util.collection.CollectionUtil;
+import study.daydayup.wolf.common.util.collection.ListUtil;
 import study.daydayup.wolf.framework.rpc.page.Page;
 import study.daydayup.wolf.framework.rpc.page.PageRequest;
 
@@ -24,34 +29,93 @@ import java.util.List;
  **/
 @Component
 public class TaskQueryRepository extends TaskRepository {
-    public List<Task> find(TaskIds taskIds) {
+    public List<Task> find(@NonNull TaskIds taskIds) {
+        taskIds.valid();
+        List<TaskDO> taskDOList = taskDAO.selectByIdIn(taskIds.getTaskIdSet(), taskIds.getOrgId());
+
+        return findDetailsByTaskList(taskDOList, taskIds);
+    }
+
+    public Page<Task> findAll(@NonNull Long orgId, @NonNull PageRequest pageRequest) {
+        Page.startPage(pageRequest.getPageNum(), pageRequest.getPageSize());
+        List<TaskDO> taskDOList ;
+
         return null;
     }
 
-    public Page<Task> findAll(@NonNull Long orgId, PageRequest pageRequest) {
+    public Page<Task> findByParent(@NonNull TaskId taskId, @NonNull PageRequest pageRequest) {
         return null;
     }
 
-    public Page<Task> findByParent(TaskId taskId, PageRequest pageRequest) {
+    public Page<Task> findByTaskType(@NonNull TaskTypeRequest typeRequest, @NonNull PageRequest pageRequest) {
         return null;
     }
 
-    public Page<Task> findByTaskType(TaskTypeRequest typeRequest, PageRequest pageRequest) {
+    public Page<Task> findByProject(@NonNull ProjectRequest typeRequest, @NonNull PageRequest pageRequest) {
         return null;
     }
 
-    public Page<Task> findByProject(ProjectRequest typeRequest, PageRequest pageRequest) {
+    public Page<Task> findByStaff(@NonNull StaffRequest staffRequest, @NonNull PageRequest pageRequest) {
         return null;
     }
 
-    public Page<Task> findByStaff(StaffRequest staffRequest, PageRequest pageRequest) {
-        return null;
+    protected TaskIds initTaskIds(List<Task> taskList, TaskOwner owner) {
+        if (CollectionUtil.isEmpty(taskList)) {
+            throw new IllegalArgumentException("taskList can't be empty");
+        }
+
+        List<Long> idList = CollectionUtil.keys(taskList, Task::getId);
+        if (CollectionUtil.isEmpty(idList)) {
+            throw new IllegalArgumentException("taskList can't be empty");
+        }
+
+        TaskIds taskIds = TaskIds.builder()
+                .orgId(owner.getOrgId())
+                .staffId(owner.getStaffId())
+                .option(owner.getOption())
+                .build();
+
+        taskIds.addAll(idList);
+        return taskIds;
     }
 
+    protected List<Task> findDetailsByTaskList(List<TaskDO> taskDOList, @NonNull TaskOwner owner) {
+        if (CollectionUtil.isEmpty(taskDOList)) {
+            return ListUtil.empty();
+        }
 
+        List<Task> taskList = TaskConverter.toModel(taskDOList);
+        if (CollectionUtil.isEmpty(taskList)) {
+            return taskList;
+        }
 
+        TaskIds taskIds = initTaskIds(taskList, owner);
+        findDetailByTaskList(taskList, taskIds);
+        findContactByTaskList(taskList, taskIds);
+        findTradeByTaskList(taskList, taskIds);
 
-    protected List<TaskEntity> findDetailsByTaskList(List<TaskDO> taskDOList, @NonNull TaskOwner owner) {
-        return null;
+        return taskList;
     }
+
+    protected void findDetailByTaskList(@NonNull List<Task> taskList, @NonNull TaskIds taskIds) {
+        detailRepository.find(taskIds, taskList);
+    }
+
+    protected void findContactByTaskList(@NonNull List<Task> taskList, @NonNull TaskIds taskIds) {
+        List<TaskContact> contactList = contactRepository.find(taskIds);
+        if (CollectionUtil.isEmpty(contactList)) {
+            return;
+        }
+
+        CollectionUtil.join(taskList, Task::getId, Task::setContact, contactList, TaskContact::getTaskId);
+    }
+
+    protected void findTradeByTaskList(@NonNull List<Task> taskList, @NonNull TaskIds taskIds) {
+        List<TaskTrade> tradeList = tradeRepository.find(taskIds);
+        if (CollectionUtil.isEmpty(tradeList)) {
+            return;
+        }
+        CollectionUtil.join(taskList, Task::getId, Task::setTrade, tradeList, TaskTrade::getTaskId);
+    }
+
 }
