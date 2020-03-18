6a9d53da078047438c8a965859e3c8e668c6b193
思源 <winglechen@gmail.com>
2020-03-18 18:39:59 +0800

    delete project_id for project_detail table

--- business/org/org-biz/src/main/java/study/daydayup/wolf/business/org/biz/task/dal/dataobject/ProjectDetailDO.java
+++ business/org/org-biz/src/main/java/study/daydayup/wolf/business/org/biz/task/dal/dataobject/ProjectDetailDO.java
@@ -10,8 +10,6 @@ public class ProjectDetailDO implements Serializable {
 
     private Long orgId;
 
-    private Long projectId;
-
     private String detail;
 
     private Byte deleteFlag;
@@ -28,7 +26,6 @@ public class ProjectDetailDO implements Serializable {
         sb.append("Hash = ").append(hashCode());
         sb.append(", id=").append(id);
         sb.append(", orgId=").append(orgId);
-        sb.append(", projectId=").append(projectId);
         sb.append(", detail=").append(detail);
         sb.append(", deleteFlag=").append(deleteFlag);
         sb.append(", createdAt=").append(createdAt);
@@ -50,7 +47,6 @@ public class ProjectDetailDO implements Serializable {
         ProjectDetailDO other = (ProjectDetailDO) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
             && (this.getOrgId() == null ? other.getOrgId() == null : this.getOrgId().equals(other.getOrgId()))
-            && (this.getProjectId() == null ? other.getProjectId() == null : this.getProjectId().equals(other.getProjectId()))
             && (this.getDetail() == null ? other.getDetail() == null : this.getDetail().equals(other.getDetail()))
             && (this.getDeleteFlag() == null ? other.getDeleteFlag() == null : this.getDeleteFlag().equals(other.getDeleteFlag()))
             && (this.getCreatedAt() == null ? other.getCreatedAt() == null : this.getCreatedAt().equals(other.getCreatedAt()));
@@ -62,7 +58,6 @@ public class ProjectDetailDO implements Serializable {
         int result = 1;
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
         result = prime * result + ((getOrgId() == null) ? 0 : getOrgId().hashCode());
-        result = prime * result + ((getProjectId() == null) ? 0 : getProjectId().hashCode());
         result = prime * result + ((getDetail() == null) ? 0 : getDetail().hashCode());
         result = prime * result + ((getDeleteFlag() == null) ? 0 : getDeleteFlag().hashCode());
         result = prime * result + ((getCreatedAt() == null) ? 0 : getCreatedAt().hashCode());
