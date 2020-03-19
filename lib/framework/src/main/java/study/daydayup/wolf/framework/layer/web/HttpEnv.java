f75a0f30cc865c460d1c997fa137f4f2c29a3113
思源 <winglechen@gmail.com>
2020-03-19 16:47:44 +0800

    add HttpEnv

---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/layer/web/HttpEnv.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.framework.layer.web;
+
+import study.daydayup.wolf.framework.layer.api.DTO;
+
+/**
+ * study.daydayup.wolf.framework.layer.web
+ *
+ * @author Wingle
+ * @since 2020/3/19 4:31 下午
+ **/
+public class HttpEnv implements DTO {
+}
