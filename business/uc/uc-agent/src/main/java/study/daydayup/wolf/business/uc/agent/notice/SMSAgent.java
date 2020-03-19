42cb9b3816c6a6c8571b6f878b703b18720b9b05
思源 <winglechen@gmail.com>
2020-03-19 16:49:31 +0800

    add notice module

---
+++ business/uc/uc-agent/src/main/java/study/daydayup/wolf/business/uc/agent/notice/SMSAgent.java
@@ -0,0 +1,14 @@
+package study.daydayup.wolf.business.uc.agent.notice;
+
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.framework.layer.api.Agent;
+
+/**
+ * study.daydayup.wolf.business.uc.agent.notice
+ *
+ * @author Wingle
+ * @since 2020/3/19 4:36 下午
+ **/
+@Component
+public class SMSAgent implements Agent {
+}
---
+++ business/uc/uc-api/src/main/java/study/daydayup/wolf/business/uc/api/notice/service/SMSService.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.business.uc.api.notice.service;
+
+import study.daydayup.wolf.framework.layer.domain.Service;
+
+/**
+ * study.daydayup.wolf.business.uc.api.notice.service
+ *
+ * @author Wingle
+ * @since 2020/3/19 4:33 下午
+ **/
+public interface SMSService extends Service {
+}
