fe279b87349d4d1cf473f98dc8fd34f7ccd0cb02
思源 <winglechen@gmail.com>
2020-03-20 16:38:32 +0800

    add config interface

---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/layer/api/Config.java
@@ -0,0 +1,12 @@
+package study.daydayup.wolf.framework.layer.api;
+
+import java.io.Serializable;
+
+/**
+ * study.daydayup.wolf.framework.layer.api
+ *
+ * @author Wingle
+ * @since 2020/1/9 1:19 下午
+ **/
+public interface Config extends Serializable {
+}
