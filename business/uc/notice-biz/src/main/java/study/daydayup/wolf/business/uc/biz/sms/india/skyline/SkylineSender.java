dd829a34956a7a8bba258924f347024e144e0946
思源 <winglechen@gmail.com>
2020-03-20 17:48:51 +0800

    add SkylineSender send flow methods

--- business/uc/notice-biz/src/main/java/study/daydayup/wolf/business/uc/biz/sms/india/skyline/SkylineSender.java
+++ business/uc/notice-biz/src/main/java/study/daydayup/wolf/business/uc/biz/sms/india/skyline/SkylineSender.java
@@ -1,8 +1,12 @@
 package study.daydayup.wolf.business.uc.biz.sms.india.skyline;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.uc.api.notice.config.SMSConfig;
 import study.daydayup.wolf.business.uc.biz.domain.service.sms.AbstractSender;
 import study.daydayup.wolf.business.uc.biz.domain.service.sms.Sender;
+
+import javax.annotation.Resource;
 
 /**
  * study.daydayup.wolf.business.uc.biz.sms.india.skyline
@@ -11,9 +15,34 @@ import study.daydayup.wolf.business.uc.biz.domain.service.sms.Sender;
  * @since 2020/3/19 7:41 下午
  **/
 @Component
+@Slf4j
 public class SkylineSender extends AbstractSender implements Sender {
+    @Resource
+    private SMSConfig config;
+
     @Override
     public int send(String mobile, String msg) {
+        validConfig();
+        prepareRequest();
+        sendSms();
+        parseResponse();
+
         return 0;
+    }
+
+    private void validConfig() {
+
+    }
+
+    private void prepareRequest() {
+
+    }
+
+    private void sendSms() {
+
+    }
+
+    private void parseResponse() {
+
     }
 }
