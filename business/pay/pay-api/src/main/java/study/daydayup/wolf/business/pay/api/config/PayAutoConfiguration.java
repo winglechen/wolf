19924fd7b2b66a15671b7c2065a1a3da319a8e5d
思源 <winglechen@gmail.com>
2020-03-20 16:37:35 +0800

    add PayAuthoConfiguration

---
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/config/PayAutoConfiguration.java
@@ -0,0 +1,14 @@
+package study.daydayup.wolf.business.pay.api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * study.daydayup.wolf.business.pay.api.config
+ *
+ * @author Wingle
+ * @since 2020/3/20 4:26 下午
+ **/
+
+@Configuration
+public class PayAutoConfiguration {
+}
