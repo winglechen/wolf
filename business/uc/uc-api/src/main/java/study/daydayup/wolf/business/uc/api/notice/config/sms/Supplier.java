e420f8a60ed19c6ee5311f52caa787bfcab39d31
思源 <winglechen@gmail.com>
2020-03-20 16:38:03 +0800

    add sms config model

---
+++ business/uc/uc-api/src/main/java/study/daydayup/wolf/business/uc/api/notice/config/sms/Supplier.java
@@ -0,0 +1,27 @@
+package study.daydayup.wolf.business.uc.api.notice.config.sms;
+
+import study.daydayup.wolf.framework.layer.api.Config;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Map;
+
+/**
+ * study.daydayup.wolf.business.uc.api.notice.config
+ *
+ * @author Wingle
+ * @since 2020/3/20 3:07 下午
+ **/
+public class Supplier implements Config {
+    @NotBlank
+    private String appId;
+    @NotBlank
+    private String appSecret;
+
+    @NotBlank
+    private String sendUrl;
+    private String queryUrl;
+    private String xxxUrl;
+
+    private Map<String, String> templateMap;
+
+}
---
+++ business/uc/uc-api/src/main/java/study/daydayup/wolf/business/uc/api/notice/domain/enums/SMSTypeEnum.java
@@ -0,0 +1,25 @@
+package study.daydayup.wolf.business.uc.api.notice.domain.enums;
+
+import lombok.Getter;
+import study.daydayup.wolf.common.lang.enums.CodeBasedEnum;
+
+/**
+ * study.daydayup.wolf.business.trade.api.domain.enums
+ * range(20 ~ 30)
+ * @author Wingle
+ * @since 2019/10/5 11:07 AM
+ **/
+@Getter
+public enum SMSTypeEnum implements CodeBasedEnum {
+    TRADE(2, "notice"),
+    NOTICE(2, "notice"),
+    CAPTCHA(1, "captcha")
+    ;
+
+    private int code;
+    private String name;
+    SMSTypeEnum(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
