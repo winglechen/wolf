c5598c4db802404d5cb031842d2519477b89213a
思源 <winglechen@gmail.com>
2020-03-21 09:30:18 +0800

    add auto configuration for notice agent

---
+++ middleware/notice/notice-agent/src/main/java/study/daydayup/wolf/middleware/notice/agent/config/NoticeAutoConfiguration.java
@@ -0,0 +1,15 @@
+package study.daydayup.wolf.middleware.notice.agent.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * study.daydayup.wolf.middleware.notice.agent.config
+ *
+ * @author Wingle
+ * @since 2020/3/21 9:27 上午
+ **/
+@Configuration
+@ComponentScan("study.daydayup.wolf.middleware.notice.agent")
+public class NoticeAutoConfiguration {
+}
