81ec8a4e9c9504a86e2a5d35665b8ed15da2d166
思源 <winglechen@gmail.com>
2020-03-22 21:38:25 +0800

    impl find of aadhaarService and bankcardService

--- business/uc/crm-biz/src/main/java/study/daydayup/wolf/business/uc/crm/biz/customer/info/api/india/AadhaarServiceImpl.java
+++ business/uc/crm-biz/src/main/java/study/daydayup/wolf/business/uc/crm/biz/customer/info/api/india/AadhaarServiceImpl.java
@@ -1,8 +1,11 @@
 package study.daydayup.wolf.business.uc.crm.biz.customer.info.api.india;
 
+import lombok.NonNull;
 import study.daydayup.wolf.business.uc.api.crm.customer.info.entity.india.Aadhaar;
 import study.daydayup.wolf.business.uc.api.crm.customer.info.service.india.AadhaarService;
+import study.daydayup.wolf.business.uc.crm.biz.customer.info.converter.india.AadhaarConverter;
 import study.daydayup.wolf.business.uc.crm.biz.customer.info.dal.dao.AadhaarDAO;
+import study.daydayup.wolf.business.uc.crm.biz.customer.info.dal.dataobject.AadhaarDO;
 import study.daydayup.wolf.framework.rpc.Result;
 import study.daydayup.wolf.framework.rpc.RpcService;
 
@@ -19,9 +22,10 @@ public class AadhaarServiceImpl implements AadhaarService {
     @Resource
     private AadhaarDAO dao;
     @Override
-    public Result<Aadhaar> find(Long accountId, Long orgId) {
-
-        return null;
+    public Result<Aadhaar> find(@NonNull Long accountId, @NonNull Long orgId) {
+        AadhaarDO aadhaarDO = dao.selectByAccountIdAndOrgId(accountId, orgId);
+        Aadhaar aadhaar = AadhaarConverter.toModel(aadhaarDO);
+        return Result.ok(aadhaar);
     }
 
     @Override
--- business/uc/crm-biz/src/main/java/study/daydayup/wolf/business/uc/crm/biz/customer/info/api/india/BankCardServiceImpl.java
+++ business/uc/crm-biz/src/main/java/study/daydayup/wolf/business/uc/crm/biz/customer/info/api/india/BankCardServiceImpl.java
@@ -1,8 +1,11 @@
 package study.daydayup.wolf.business.uc.crm.biz.customer.info.api.india;
 
+import lombok.NonNull;
 import study.daydayup.wolf.business.uc.api.crm.customer.info.entity.india.BankCard;
 import study.daydayup.wolf.business.uc.api.crm.customer.info.service.india.BankCardService;
+import study.daydayup.wolf.business.uc.crm.biz.customer.info.converter.india.BankCardConverter;
 import study.daydayup.wolf.business.uc.crm.biz.customer.info.dal.dao.BankCardDAO;
+import study.daydayup.wolf.business.uc.crm.biz.customer.info.dal.dataobject.BankCardDO;
 import study.daydayup.wolf.framework.rpc.Result;
 import study.daydayup.wolf.framework.rpc.RpcService;
 
@@ -20,8 +23,10 @@ public class BankCardServiceImpl implements BankCardService {
     private BankCardDAO dao;
 
     @Override
-    public Result<BankCard> find(Long accountId, Long orgId) {
-        return null;
+    public Result<BankCard> find(@NonNull Long accountId, @NonNull Long orgId) {
+        BankCardDO cardDO = dao.selectByAccountIdAndOrgId(accountId, orgId);
+        BankCard card = BankCardConverter.toModel(cardDO);
+        return Result.ok(card);
     }
 
     @Override
