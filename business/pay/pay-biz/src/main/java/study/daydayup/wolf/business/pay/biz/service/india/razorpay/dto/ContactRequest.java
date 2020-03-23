b008c4c4552529c8bc5cc8213cd95ecff28aac26
思源 <winglechen@gmail.com>
2020-03-23 11:50:28 +0800

    add ContactRequest

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/service/india/razorpay/dto/ContactRequest.java
@@ -0,0 +1,23 @@
+package study.daydayup.wolf.business.pay.biz.service.india.razorpay.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import study.daydayup.wolf.framework.layer.api.Request;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.service.india.razorpay.dto
+ *
+ * @author Wingle
+ * @since 2020/3/23 3:13 上午
+ **/
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactRequest implements Request {
+    private String name;
+    private String type;
+    private String referenceId;
+}
