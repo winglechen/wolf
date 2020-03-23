0e307fe5e9be12826b4897168bee2d56c241bcc1
思源 <winglechen@gmail.com>
2020-03-23 15:09:47 +0800

    add AccountEpiRequest and PayoutEpiRequest

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/service/india/razorpay/dto/PayoutEpiRequest.java
@@ -0,0 +1,21 @@
+package study.daydayup.wolf.business.pay.biz.service.india.razorpay.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import study.daydayup.wolf.framework.layer.api.Request;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.service.india.razorpay.dto
+ *
+ * @author Wingle
+ * @since 2020/3/23 3:13 上午
+ **/
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PayoutEpiRequest implements Request {
+
+}
