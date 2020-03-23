f7a89d076d5e42489cbdc54e3944eaf436fb3d20
思源 <winglechen@gmail.com>
2020-03-23 11:50:08 +0800

    init contact request

--- business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/service/india/razorpay/payout/RazorContactService.java
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/service/india/razorpay/payout/RazorContactService.java
@@ -3,6 +3,7 @@ package study.daydayup.wolf.business.pay.biz.service.india.razorpay.payout;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 import study.daydayup.wolf.business.pay.api.dto.base.payout.PayoutRequest;
+import study.daydayup.wolf.business.pay.biz.service.india.razorpay.dto.ContactRequest;
 import study.daydayup.wolf.business.pay.biz.service.india.razorpay.model.RazorAccount;
 import study.daydayup.wolf.framework.layer.domain.Service;
 
@@ -14,8 +15,28 @@ import study.daydayup.wolf.framework.layer.domain.Service;
  **/
 @Component
 public class RazorContactService implements Service {
+    private RazorAccount account;
+    private PayoutRequest request;
 
     public RazorAccount create(RazorAccount account, @Validated PayoutRequest request) {
+        initAccount(account);
+
+        return this.account;
+    }
+
+    private void initAccount(RazorAccount account) {
+        if (null == account) {
+            this.account = new RazorAccount();
+        } else {
+            this.account = account;
+        }
+    }
+
+    private ContactRequest createRequest() {
         return null;
+    }
+
+    private void callEpi() {
+
     }
 }
