3f1b4469b467182d6173c62271ade37f413ffab2
思源 <winglechen@gmail.com>
2020-03-25 23:45:39 +0800

    add empty datav controllers

---
+++ app/estar/union-admin/src/main/java/study/daydayup/wolf/business/union/admin/controller/datav/daily/DailyLoanController.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.union.admin.controller.datav.daily;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * study.daydayup.wolf.business.union.admin.controller.datav.daily
+ *
+ * @author Wingle
+ * @since 2020/3/25 11:44 下午
+ **/
+@RestController
+public class DailyLoanController {
+}
---
+++ app/estar/union-admin/src/main/java/study/daydayup/wolf/business/union/admin/controller/datav/track/LoanTrackController.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.union.admin.controller.datav.track;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * study.daydayup.wolf.business.union.admin.controller.datav.track
+ *
+ * @author Wingle
+ * @since 2020/3/25 11:43 下午
+ **/
+@RestController
+public class LoanTrackController {
+}
