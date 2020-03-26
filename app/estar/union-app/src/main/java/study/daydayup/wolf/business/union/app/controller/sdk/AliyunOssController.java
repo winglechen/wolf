698c04909a9bdf91b6cf35184f6143d2c854feab
思源 <winglechen@gmail.com>
2020-03-26 22:30:12 +0800

    add RestController to AliyunOssController

--- app/estar/union-app/src/main/java/study/daydayup/wolf/business/union/app/controller/sdk/AliyunOssController.java
+++ app/estar/union-app/src/main/java/study/daydayup/wolf/business/union/app/controller/sdk/AliyunOssController.java
@@ -1,6 +1,7 @@
 package study.daydayup.wolf.business.union.app.controller.sdk;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import study.daydayup.wolf.framework.rpc.Result;
 import study.daydayup.wolf.sdk.aliyun.oss.AliyunOssUtil;
 
@@ -13,6 +14,7 @@ import java.util.Map;
  * @author Wingle
  * @since 2020/3/26 7:42 下午
  **/
+@RestController
 public class AliyunOssController {
     @Resource
     private AliyunOssUtil ossUtil;
