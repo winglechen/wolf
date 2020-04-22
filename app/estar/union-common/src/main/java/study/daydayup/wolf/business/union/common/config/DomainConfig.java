3eb1ecd6884b8d96bac59389a0ffb41c57b4a5c1
思源 <winglechen@gmail.com>
2020-04-22 16:33:59 +0800

    add union-common module

---
+++ app/estar/union-common/src/main/java/study/daydayup/wolf/business/union/common/config/DomainConfig.java
@@ -0,0 +1,18 @@
+package study.daydayup.wolf.business.union.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+/**
+ * study.daydayup.wolf.business.union.common.config
+ *
+ * @author Wingle
+ * @since 2020/4/22 4:29 下午
+ **/
+@Configuration
+@ConfigurationProperties(prefix = "wolf.domain")
+public class DomainConfig {
+    private Map<String, String > file;
+}
