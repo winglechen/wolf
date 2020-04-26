e0fa335a3426d776360b125d83a164cf0242b4c9
思源 <winglechen@gmail.com>
2020-04-26 19:06:09 +0800

    add PayServiceNotFoundException

---
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/domain/exception/PayServiceNotFoundException.java
@@ -0,0 +1,20 @@
+package study.daydayup.wolf.business.pay.api.domain.exception;
+
+import study.daydayup.wolf.common.lang.exception.SystemException;
+import study.daydayup.wolf.common.util.lang.StringUtil;
+
+/**
+ * study.daydayup.wolf.business.goods.api.exception
+ *
+ * @author Wingle
+ * @since 2019/12/12 2:04 下午
+ **/
+public class PayServiceNotFoundException extends SystemException {
+    public PayServiceNotFoundException() {
+        super(170000, "payment method fail");
+    }
+
+    public PayServiceNotFoundException(String msg) {
+        super(170000, StringUtil.join("payment method: ", msg, " not found."));
+    }
+}
