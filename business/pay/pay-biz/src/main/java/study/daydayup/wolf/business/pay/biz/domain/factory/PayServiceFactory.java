e728a6ff1dd4c0eb499201dc483acabb447978c0
思源 <winglechen@gmail.com>
2020-04-26 18:39:30 +0800

    move PayServiceFactory to domain.factory

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/domain/factory/PayServiceFactory.java
@@ -0,0 +1,18 @@
+package study.daydayup.wolf.business.pay.biz.domain.factory;
+
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.pay.api.service.PayService;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.util
+ *
+ * @author Wingle
+ * @since 2020/4/26 6:36 下午
+ **/
+@Component
+public class PayServiceFactory {
+    public static PayService create(Integer paymentCode) {
+
+        return null;
+    }
+}
