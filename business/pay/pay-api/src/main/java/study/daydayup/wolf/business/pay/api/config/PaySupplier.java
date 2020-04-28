0c01c641a593a49a5a7fc8fe9f9463c118afd9d9
思源 <winglechen@gmail.com>
2020-04-28 16:17:14 +0800

    add pay supplier config

--- business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/config/PaySupplier.java
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/config/PaySupplier.java
@@ -7,4 +7,16 @@ package study.daydayup.wolf.business.pay.api.config;
  * @since 2020/4/26 10:54 下午
  **/
 public class PaySupplier {
+    private String appId;
+    private String appSecret;
+
+    private String version;
+    private String country;
+    private String currency;
+
+    private String returnUrl;
+    private String notifyUrl;
+
+    private String createUrl;
+    
 }
