8fd07595b1402c4ef2f9be9a412766065fa45847
思源 <winglechen@gmail.com>
2020-04-28 22:58:59 +0800

    add DokypayCreator

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/service/india/dokypay/DokypayCreator.java
@@ -0,0 +1,59 @@
+package study.daydayup.wolf.business.pay.biz.service.india.dokypay;
+
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.pay.api.config.PayConfig;
+import study.daydayup.wolf.business.pay.api.config.PaySupplier;
+import study.daydayup.wolf.business.pay.biz.domain.service.AbstractPaymentCreator;
+import study.daydayup.wolf.business.pay.biz.domain.service.PaymentCreator;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.service.india.dokypay
+ *
+ * @author Wingle
+ * @since 2020/4/28 9:37 下午
+ **/
+@Component
+public class DokypayCreator extends AbstractPaymentCreator implements PaymentCreator {
+    private static String CONFIG_KEY = "dokypay";
+    private static String PROD_NAME = "southeast.asia";
+    private PaySupplier config;
+
+    @Resource
+    private PayConfig payConfig;
+
+
+    @Override
+    public void callPayEpi() {
+        initConfig();
+
+        Map<String, Object> request = initRequest();
+
+
+    }
+
+    @Override
+    public void parseEpiResponse() {
+
+    }
+
+    private void initConfig() {
+
+    }
+
+    private Map<String, Object> initRequest() {
+        Map<String, Object> request = new HashMap<>(8);
+        request.put("appId", config.getAppId());
+        request.put("prodName", PROD_NAME);
+        request.put("version", config.getVersion());
+        request.put("country", "IN");
+        request.put("currency", "INR");
+
+        Map<String, Object> extInfo = new HashMap<>(2);
+        extInfo.put("paymentTypes","credit,debit,ewallet,upi");
+        return request;
+    }
+}
