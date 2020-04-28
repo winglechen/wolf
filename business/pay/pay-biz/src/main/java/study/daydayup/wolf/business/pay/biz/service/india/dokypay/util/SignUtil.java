c24fd3aaee603e915236f590c7c77d2261f3814c
思源 <winglechen@gmail.com>
2020-04-28 19:35:40 +0800

    add SignUtil for dokypay

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/service/india/dokypay/util/SignUtil.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.business.pay.biz.service.india.dokypay.util;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.service.india.dokypay.util
+ *
+ * @author Wingle
+ * @since 2020/4/28 7:28 下午
+ **/
+public class SignUtil {
+    public static String create() {
+        return null;
+    }
+}
