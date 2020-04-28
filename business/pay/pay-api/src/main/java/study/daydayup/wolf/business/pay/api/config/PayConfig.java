4716f97851b3eadfe6c8b0177fe5200472af9454
思源 <winglechen@gmail.com>
2020-04-28 15:09:04 +0800

    add empty PayConfig

---
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/config/PayConfig.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.pay.api.config;
+
+/**
+ * study.daydayup.wolf.business.pay.api.config
+ *
+ * @author Wingle
+ * @since 2020/4/26 10:51 下午
+ **/
+public class PayConfig {
+}
---
+++ business/pay/pay-api/src/main/java/study/daydayup/wolf/business/pay/api/config/PaySupplier.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.pay.api.config;
+
+/**
+ * study.daydayup.wolf.business.pay.api.config
+ *
+ * @author Wingle
+ * @since 2020/4/26 10:54 下午
+ **/
+public class PaySupplier {
+}
