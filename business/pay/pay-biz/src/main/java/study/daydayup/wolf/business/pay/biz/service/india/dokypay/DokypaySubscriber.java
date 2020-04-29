f21d16b3b996a874639e30746eeccb45d9e4ffb7
思源 <winglechen@gmail.com>
2020-04-29 19:03:28 +0800

    add empty DokypaySubscriber

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/service/india/dokypay/DokypaySubscriber.java
@@ -0,0 +1,18 @@
+package study.daydayup.wolf.business.pay.biz.service.india.dokypay;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.service.india.dokypay
+ *
+ * @author Wingle
+ * @since 2020/4/29 6:58 下午
+ **/
+@Slf4j
+@Component
+public class DokypaySubscriber {
+    public int subscribe(String data) {
+        return 0;
+    }
+}
