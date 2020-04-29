494b77730818bb8dee00f5967ccd8d7bbeb0adaa
思源 <winglechen@gmail.com>
2020-04-29 18:26:24 +0800

    add empty PayoutServiceFactory

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/domain/factory/PayoutServiceFactory.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.business.pay.biz.domain.factory;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.domain.factory
+ *
+ * @author Wingle
+ * @since 2020/4/29 6:25 下午
+ **/
+public class PayoutServiceFactory {
+}
