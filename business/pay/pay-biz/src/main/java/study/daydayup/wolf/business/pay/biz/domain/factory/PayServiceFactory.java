04e66e41ca5b9f68dc0605d412ea799862b38866
思源 <winglechen@gmail.com>
2020-04-29 18:35:23 +0800

    impl PayoutServiceFactory

--- business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/domain/factory/PayServiceFactory.java
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/domain/factory/PayServiceFactory.java
@@ -1,10 +1,10 @@
 package study.daydayup.wolf.business.pay.biz.domain.factory;
 
-import lombok.NonNull;
 import org.springframework.stereotype.Component;
 import study.daydayup.wolf.business.pay.api.domain.enums.PaymentMethodEnum;
 import study.daydayup.wolf.business.pay.api.domain.exception.PayServiceNotFoundException;
 import study.daydayup.wolf.business.pay.api.service.PayService;
+import study.daydayup.wolf.business.pay.api.service.india.RazorpayService;
 import study.daydayup.wolf.business.pay.biz.service.india.cashfree.CashFreeService;
 import study.daydayup.wolf.business.pay.biz.service.india.dokypay.DokypayService;
 import study.daydayup.wolf.common.util.lang.EnumUtil;
@@ -23,6 +23,8 @@ public class PayServiceFactory {
     private CashFreeService cashFreeService;
     @Resource
     private DokypayService dokypayService;
+    @Resource
+    private RazorpayService razorpayService;
 
 
     public PayService create(int paymentCode) {
@@ -33,6 +35,8 @@ public class PayServiceFactory {
                 return cashFreeService;
             case DOKYPAY:
                 return dokypayService;
+            case RAZORPAY:
+                return razorpayService;
             default:
                 throw new PayServiceNotFoundException();
         }
--- business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/domain/factory/PayoutServiceFactory.java
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/domain/factory/PayoutServiceFactory.java
@@ -1,10 +1,43 @@
 package study.daydayup.wolf.business.pay.biz.domain.factory;
 
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.pay.api.domain.enums.PaymentMethodEnum;
+import study.daydayup.wolf.business.pay.api.domain.exception.PayoutServiceNotFoundException;
+import study.daydayup.wolf.business.pay.api.service.PayoutService;
+import study.daydayup.wolf.business.pay.api.service.india.RazorpayService;
+import study.daydayup.wolf.business.pay.biz.service.india.cashfree.CashFreeService;
+import study.daydayup.wolf.business.pay.biz.service.india.dokypay.DokypayService;
+import study.daydayup.wolf.common.util.lang.EnumUtil;
+
+import javax.annotation.Resource;
+
 /**
  * study.daydayup.wolf.business.pay.biz.domain.factory
  *
  * @author Wingle
  * @since 2020/4/29 6:25 下午
  **/
+@Component
 public class PayoutServiceFactory {
+    @Resource
+    private CashFreeService cashFreeService;
+    @Resource
+    private DokypayService dokypayService;
+    @Resource
+    private RazorpayService razorpayService;
+
+    public PayoutService create(int paymentCode) {
+        PaymentMethodEnum paymentMethod = EnumUtil.codeOf(paymentCode, PaymentMethodEnum.class);
+
+        switch (paymentMethod) {
+            case CASEFREE:
+                return cashFreeService;
+            case DOKYPAY:
+                return dokypayService;
+            case RAZORPAY:
+                return razorpayService;
+            default:
+                throw new PayoutServiceNotFoundException();
+        }
+    }
 }
