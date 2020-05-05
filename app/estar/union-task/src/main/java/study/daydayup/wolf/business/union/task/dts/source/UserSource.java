0a40728c0fba1a6c191e3eec5ceb1630a8e9ea6b
思源 <winglechen@gmail.com>
2020-05-05 11:53:50 +0800

    add UserSource

---
+++ app/estar/union-task/src/main/java/study/daydayup/wolf/business/union/task/dts/source/UserSource.java
@@ -0,0 +1,35 @@
+package study.daydayup.wolf.business.union.task.dts.source;
+
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.union.task.config.ShardingConfig;
+import study.daydayup.wolf.common.io.db.Table;
+import study.daydayup.wolf.dts.config.SourceConfig;
+import study.daydayup.wolf.dts.source.MysqlSource;
+
+import javax.annotation.Resource;
+
+/**
+ * study.daydayup.wolf.business.union.task.dts.source
+ *
+ * @author Wingle
+ * @since 2020/5/5 11:22 上午
+ **/
+@Component
+public class UserSource {
+    @Resource
+    private ShardingConfig shardingConfig;
+    @Resource
+    private MysqlSource mysqlSource;
+
+    public MysqlSource  findLatestUser() {
+        SourceConfig sourceConfig = SourceConfig.builder()
+                .sourceName("latest-user")
+                .tableName("user")
+                .columns("id, org_id, account_id, created_at")
+                .shardingKey(shardingConfig.getShard())
+                .build();
+        mysqlSource.init(sourceConfig);
+
+        return mysqlSource;
+    }
+}
