127540bd8102155fc4b73af232903b148fb38e8e
思源 <winglechen@gmail.com>
2020-05-05 16:15:20 +0800

    add DailyTradeService

---
+++ app/estar/union-task/src/main/java/study/daydayup/wolf/business/union/task/service/DailyTradeService.java
@@ -0,0 +1,14 @@
+package study.daydayup.wolf.business.union.task.service;
+
+/**
+ * study.daydayup.wolf.business.union.task.service
+ *
+ * @author Wingle
+ * @since 2020/5/5 4:13 下午
+ **/
+public interface DailyTradeService {
+    void countNewContract();
+    void countNewOrder();
+    void countPaidOrder();
+
+}
---
+++ app/estar/union-task/src/main/java/study/daydayup/wolf/business/union/task/service/impl/DailyTradeServiceImpl.java
@@ -0,0 +1,28 @@
+package study.daydayup.wolf.business.union.task.service.impl;
+
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.union.task.service.DailyTradeService;
+
+/**
+ * study.daydayup.wolf.business.union.task.service.impl
+ *
+ * @author Wingle
+ * @since 2020/5/5 4:14 下午
+ **/
+@Component
+public class DailyTradeServiceImpl implements DailyTradeService {
+    @Override
+    public void countNewContract() {
+        
+    }
+
+    @Override
+    public void countNewOrder() {
+
+    }
+
+    @Override
+    public void countPaidOrder() {
+
+    }
+}
