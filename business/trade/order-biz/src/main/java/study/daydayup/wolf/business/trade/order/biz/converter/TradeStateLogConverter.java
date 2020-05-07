c00a9d3616ad0ac319e0f5b9203b74e1af383741
思源 <winglechen@gmail.com>
2020-05-07 18:07:19 +0800

    add TradeStateLogConverter

---
+++ business/trade/order-biz/src/main/java/study/daydayup/wolf/business/trade/order/biz/converter/TradeStateLogConverter.java
@@ -0,0 +1,44 @@
+package study.daydayup.wolf.business.trade.order.biz.converter;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.trade.api.domain.entity.trade.TradeStateLog;
+import study.daydayup.wolf.business.trade.order.biz.dal.dataobject.TradeStateLogDO;
+import study.daydayup.wolf.common.util.collection.CollectionUtil;
+import study.daydayup.wolf.framework.layer.converter.Converter;
+
+import java.util.List;
+
+/**
+ * study.daydayup.wolf.business.trade.order.biz.converter
+ *
+ * @author Wingle
+ * @since 2020/1/17 10:07 下午
+ **/
+public class TradeStateLogConverter implements Converter {
+    public static TradeStateLogDO toDo(TradeStateLog stateLog) {
+        if (stateLog == null) {
+            return null;
+        }
+
+        TradeStateLogDO stateLogDO = new TradeStateLogDO();
+        BeanUtils.copyProperties(stateLog, stateLogDO);
+
+        return stateLogDO;
+    }
+
+    public static TradeStateLog toModel(TradeStateLogDO stateLogDO) {
+        if (stateLogDO == null) {
+            return null;
+        }
+
+        TradeStateLog stateLog = new TradeStateLog();
+        BeanUtils.copyProperties(stateLogDO, stateLog);
+
+        return stateLog;
+    }
+
+    public static List<TradeStateLog> toModel(List<TradeStateLogDO> stateLogDOList) {
+        return CollectionUtil.to(stateLogDOList, TradeStateLogConverter::toModel);
+    }
+}
