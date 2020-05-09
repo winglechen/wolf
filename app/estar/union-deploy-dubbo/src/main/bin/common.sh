1ccc7202407ae221cd3e7826208c2af3fcfbef49
思源 <winglechen@gmail.com>
2020-05-09 15:35:38 +0800

    add source /etc/profile for start.sh

--- app/estar/union-deploy-dubbo/src/main/bin/common.sh
+++ app/estar/union-deploy-dubbo/src/main/bin/common.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 APPLICATION_NAME="@project.name@"
 
+source /etc/profile
+source ~/.bashrc
+
 CURRENT_PATH=$(cd `dirname $0`; pwd)
 PROJECT_PATH=$(cd `dirname $0`; cd ../ ; pwd)
 BIN_PATH="${PROJECT_PATH}/bin"
@@ -11,13 +14,13 @@ BIN_PATH="${PROJECT_PATH}/bin"
 #fi
 
 ## set java path
-#JAVA=$(which java)
-#if [ -z "$JAVA" ]; then
-#  	echo "Cannot find a Java JDK. Please set either set JAVA or put java (>=1.8) in your PATH." 2>&2
-#    exit 1
-#fi
+JAVA=$(which java)
+if [ -z "$JAVA" ]; then
+  	echo "Cannot find a Java JDK. Please set either set JAVA or put java (>=1.8) in your PATH." 2>&2
+    exit 1
+fi
 
-JAVA="${JAVA_HOME}/bin/java"
+#JAVA="${JAVA_HOME}/bin/java"
 
 MAIN_JAR=${PROJECT_PATH}/lib/${APPLICATION_NAME}.jar
 APP_PID=`ps -ef | grep "$MAIN_JAR" | grep -v grep | awk '{print $2}'`
