27fc1c3a5c832111a1a4edd645123ada9c0b2cf0
思源 <winglechen@gmail.com>
2020-05-09 20:59:19 +0800

    revert start script

--- app/estar/union-deploy-dubbo/src/main/bin/common.sh
+++ app/estar/union-deploy-dubbo/src/main/bin/common.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 APPLICATION_NAME="@project.name@"
 
-source /etc/profile
-source ~/.bashrc
-
 CURRENT_PATH=$(cd `dirname $0`; pwd)
 PROJECT_PATH=$(cd `dirname $0`; cd ../ ; pwd)
 BIN_PATH="${PROJECT_PATH}/bin"
@@ -19,8 +16,6 @@ if [ -z "$JAVA" ]; then
   	echo "Cannot find a Java JDK. Please set either set JAVA or put java (>=1.8) in your PATH." 2>&2
     exit 1
 fi
-
-#JAVA="${JAVA_HOME}/bin/java"
 
 MAIN_JAR=${PROJECT_PATH}/lib/${APPLICATION_NAME}.jar
 APP_PID=`ps -ef | grep "$MAIN_JAR" | grep -v grep | awk '{print $2}'`
