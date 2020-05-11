72270117037a9547180f3a775d7eb9b2b6f292e9
思源 <winglechen@gmail.com>
2020-05-11 16:54:09 +0800

    add ChannelEnum

---
+++ app/estar/union-app/src/main/java/study/daydayup/wolf/business/union/app/ChannelEnum.java
@@ -0,0 +1,25 @@
+package study.daydayup.wolf.business.union.app;
+
+import lombok.Getter;
+import study.daydayup.wolf.common.lang.enums.CodeBasedEnum;
+
+/**
+ * study.daydayup.wolf.business.union.app
+ *
+ * @author Wingle
+ * @since 2020/5/11 4:43 下午
+ **/
+@Getter
+public enum  ChannelEnum implements CodeBasedEnum {
+    FACEBOOK(990002, "facebook"),
+    GOOGLE(990001, "google"),
+    ;
+
+    private int code;
+    private String name;
+
+    ChannelEnum(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
