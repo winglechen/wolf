b52c91a32e16993cd83f6f1ffbe5cac4a2f68138
思源 <winglechen@gmail.com>
2020-05-15 12:16:24 +0800

    add empty DubboRequestScope and DubboThreadScope

---
+++ lib/framework/src/main/java/study/daydayup/wolf/framework/rpc/dubbo/beans/DubboThreadScope.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.framework.rpc.dubbo.beans;
+
+/**
+ * study.daydayup.wolf.framework.rpc.dubbo.beans
+ *
+ * @author Wingle
+ * @since 2020/5/15 11:38 上午
+ **/
+public class DubboThreadScope {
+}
