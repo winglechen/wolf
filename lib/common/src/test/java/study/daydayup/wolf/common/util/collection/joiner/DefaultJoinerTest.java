7f85663d7b219b452be490663820c0ae8e409fd3
思源 <winglechen@gmail.com>
2020-05-18 21:31:17 +0800

    add empty Joiner test files

---
+++ lib/common/src/test/java/study/daydayup/wolf/common/util/collection/joiner/DefaultJoinerTest.java
@@ -0,0 +1,18 @@
+package study.daydayup.wolf.common.util.collection.joiner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * study.daydayup.wolf.common.util.collection.joiner
+ *
+ * @author Wingle
+ * @since 2020/5/18 9:30 下午
+ **/
+public class DefaultJoinerTest {
+
+    @Test
+    public void join() {
+    }
+}
