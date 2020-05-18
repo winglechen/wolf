4e7ed796f3d70dbdf019e4c4323c18c50582572c
思源 <winglechen@gmail.com>
2020-05-18 15:04:34 +0800

    add empty CollectionJoiner

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/util/collection/joiner/CollectionJoiner.java
@@ -0,0 +1,13 @@
+package study.daydayup.wolf.common.util.collection.joiner;
+
+import java.util.Collection;
+
+/**
+ * study.daydayup.wolf.common.util.collection
+ *
+ * @author Wingle
+ * @since 2020/5/18 3:01 下午
+ **/
+public class CollectionJoiner {
+
+}
