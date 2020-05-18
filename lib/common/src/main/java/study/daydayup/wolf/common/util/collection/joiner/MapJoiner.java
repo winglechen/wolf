11547b72cfb2991384529f09a6671ebcbea68ff9
思源 <winglechen@gmail.com>
2020-05-18 21:09:04 +0800

    add empty MapJoiner

---
+++ lib/common/src/main/java/study/daydayup/wolf/common/util/collection/joiner/MapJoiner.java
@@ -0,0 +1,10 @@
+package study.daydayup.wolf.common.util.collection.joiner;
+
+/**
+ * study.daydayup.wolf.common.util.collection.joiner
+ *
+ * @author Wingle
+ * @since 2020/5/18 9:08 下午
+ **/
+public class MapJoiner {
+}
