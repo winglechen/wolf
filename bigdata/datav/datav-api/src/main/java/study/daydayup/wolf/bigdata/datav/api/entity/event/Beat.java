87db172d8018ef407cfad9f2b1e7be089e8d8c4e
思源 <winglechen@gmail.com>
2020-06-02 10:57:51 +0800

    add Beat model

---
+++ bigdata/datav/datav-api/src/main/java/study/daydayup/wolf/bigdata/datav/api/entity/event/Beat.java
@@ -0,0 +1,29 @@
+package study.daydayup.wolf.bigdata.datav.api.entity.event;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class Beat implements Serializable {
+    private Long id;
+
+    private Long orgId;
+    private Long accountId;
+
+    private String deviceId;
+    private String deviceType;
+
+    private String ip;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+    private String eventType;
+    private String event;
+    private String eventContext;
+    private String eventMemo;
+
+    private LocalDateTime createdAt;
+}
