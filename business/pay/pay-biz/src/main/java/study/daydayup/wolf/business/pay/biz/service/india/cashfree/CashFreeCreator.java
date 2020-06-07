3084eeb033096cf621b68400d704e6890e12702e
思源 <winglechen@gmail.com>
2020-06-07 22:02:18 +0800

    add empty CashFreeCreator

---
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/service/india/cashfree/CashFreeCreator.java
@@ -0,0 +1,22 @@
+package study.daydayup.wolf.business.pay.biz.service.india.cashfree;
+
+import study.daydayup.wolf.business.pay.biz.domain.service.AbstractPaymentCreator;
+import study.daydayup.wolf.business.pay.biz.domain.service.PaymentCreator;
+
+/**
+ * study.daydayup.wolf.business.pay.biz.service.india.cashfree
+ *
+ * @author Wingle
+ * @since 2020/6/7 10:01 下午
+ **/
+public class CashFreeCreator extends AbstractPaymentCreator implements PaymentCreator {
+    @Override
+    public void callPayEpi() {
+        
+    }
+
+    @Override
+    public void parseEpiResponse() {
+
+    }
+}
