52d6731ffb79c15f3d79be5d2de141f8659841f7
思源 <winglechen@gmail.com>
2020-06-20 17:27:13 +0800

    add DailySmsService

---
+++ app/estar/union-task/src/main/java/study/daydayup/wolf/business/union/task/service/DailySmsService.java
@@ -0,0 +1,11 @@
+package study.daydayup.wolf.business.union.task.service;
+
+/**
+ * study.daydayup.wolf.business.union.task.service
+ *
+ * @author Wingle
+ * @since 2020/6/20 5:25 下午
+ **/
+public interface DailySmsService {
+    void countVerifyCodeSms();
+}
---
+++ app/estar/union-task/src/main/java/study/daydayup/wolf/business/union/task/service/impl/DailySmsServiceImpl.java
@@ -0,0 +1,18 @@
+package study.daydayup.wolf.business.union.task.service.impl;
+
+import org.springframework.stereotype.Component;
+import study.daydayup.wolf.business.union.task.service.DailySmsService;
+
+/**
+ * study.daydayup.wolf.business.union.task.service.impl
+ *
+ * @author Wingle
+ * @since 2020/6/20 5:26 下午
+ **/
+@Component
+public class DailySmsServiceImpl implements DailySmsService {
+    @Override
+    public void countVerifyCodeSms() {
+
+    }
+}
