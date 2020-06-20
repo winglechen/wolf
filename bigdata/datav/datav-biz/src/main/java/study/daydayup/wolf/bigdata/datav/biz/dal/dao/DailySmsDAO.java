687f758201464e3664805cd08656dbf021581f94
思源 <winglechen@gmail.com>
2020-06-20 17:18:19 +0800

    add selectByDate for DailyTradeDAO

--- bigdata/datav/datav-biz/src/main/java/study/daydayup/wolf/bigdata/datav/biz/dal/dao/DailySmsDAO.java
+++ bigdata/datav/datav-biz/src/main/java/study/daydayup/wolf/bigdata/datav/biz/dal/dao/DailySmsDAO.java
@@ -1,6 +1,10 @@
 package study.daydayup.wolf.bigdata.datav.biz.dal.dao;
 
+import org.apache.ibatis.annotations.Param;
+import study.daydayup.wolf.bigdata.datav.api.dto.daily.DateRangeRequest;
 import study.daydayup.wolf.bigdata.datav.biz.dal.dataobject.DailySmsDO;
+
+import java.util.List;
 
 public interface DailySmsDAO {
     int deleteById(Long id);
@@ -14,4 +18,7 @@ public interface DailySmsDAO {
     int updateByIdSelective(DailySmsDO record);
 
     int updateById(DailySmsDO record);
+
+    List<DailySmsDO> selectByDate(@Param("request") DateRangeRequest request);
+
 }
