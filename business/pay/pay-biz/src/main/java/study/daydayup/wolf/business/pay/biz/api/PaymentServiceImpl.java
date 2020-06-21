4174556e7eeacbd52c6f501cb905a4b1bae9ec8c
思源 <winglechen@gmail.com>
2020-06-21 18:05:19 +0800

    impl PaymentServiceImpl.query.dispatcher

--- business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/api/PaymentServiceImpl.java
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/api/PaymentServiceImpl.java
@@ -1,12 +1,19 @@
 package study.daydayup.wolf.business.pay.biz.api;
 
+import lombok.NonNull;
+import org.springframework.validation.annotation.Validated;
 import study.daydayup.wolf.business.pay.api.domain.entity.Payment;
 import study.daydayup.wolf.business.pay.api.dto.base.pay.PaymentQuery;
 import study.daydayup.wolf.business.pay.api.service.PaymentService;
+import study.daydayup.wolf.business.pay.biz.dal.dao.PaymentDAO;
+import study.daydayup.wolf.common.util.lang.StringUtil;
 import study.daydayup.wolf.framework.rpc.Result;
 import study.daydayup.wolf.framework.rpc.RpcService;
 import study.daydayup.wolf.framework.rpc.page.Page;
 import study.daydayup.wolf.framework.rpc.page.PageRequest;
+
+import javax.annotation.Resource;
+import javax.validation.constraints.NotNull;
 
 /**
  * study.daydayup.wolf.business.pay.biz.api
@@ -16,8 +23,50 @@ import study.daydayup.wolf.framework.rpc.page.PageRequest;
  **/
 @RpcService
 public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDAO paymentDAO;
     @Override
-    public Result<Page<Payment>> query(PaymentQuery query, PageRequest pageRequest) {
+    public Result<Page<Payment>> query(@Validated PaymentQuery query, PageRequest pageRequest) {
+        if (StringUtil.notBlank(query.getPaymentNo())) {
+            return byPaymentNo(query.getPaymentNo(), query.getPayeeId());
+        }
+
+        if (StringUtil.notBlank(query.getTradeNo())) {
+            return byPaymentNo(query.getTradeNo(), query.getPayeeId());
+        }
+
+        if (StringUtil.notBlank(query.getOutTradeNo())) {
+            return byPaymentNo(query.getOutTradeNo(), query.getPayeeId());
+        }
+
+        if (null != query.getCreatedStart() || null != query.getCreatedEnd()) {
+            return byRange(query, pageRequest);
+        }
+
+        return byState(query, pageRequest);
+    }
+
+    private Result<Page<Payment>> byPaymentNo(@NonNull String paymentNo, @NonNull Long payeeId) {
         return null;
     }
+
+    private Result<Page<Payment>> byTradeNo(@NonNull String tradeNo, @NonNull Long payeeId) {
+        return null;
+    }
+
+    private Result<Page<Payment>> byOutTradeNo(@NonNull String outTradeNo, @NonNull Long payeeId) {
+        return null;
+    }
+
+    private Result<Page<Payment>> byState(@Validated PaymentQuery query, PageRequest pageRequest) {
+        return null;
+    }
+
+    private Result<Page<Payment>> byRange(@Validated PaymentQuery query, PageRequest pageRequest) {
+        return null;
+    }
+
+
+
+
 }
