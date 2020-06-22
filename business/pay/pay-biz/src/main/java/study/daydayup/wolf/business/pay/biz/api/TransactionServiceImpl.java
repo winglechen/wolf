6693f4d546a5634e38ee348039cf0f5a9df683f2
思源 <winglechen@gmail.com>
2020-06-22 09:50:29 +0800

    impl TransactionServiceImpl.dispatcher

--- business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/api/TransactionServiceImpl.java
+++ business/pay/pay-biz/src/main/java/study/daydayup/wolf/business/pay/biz/api/TransactionServiceImpl.java
@@ -1,8 +1,10 @@
 package study.daydayup.wolf.business.pay.biz.api;
 
+import lombok.NonNull;
 import study.daydayup.wolf.business.pay.api.domain.entity.Transaction;
 import study.daydayup.wolf.business.pay.api.dto.base.manage.TransactionQuery;
 import study.daydayup.wolf.business.pay.api.service.TransactionService;
+import study.daydayup.wolf.common.util.lang.StringUtil;
 import study.daydayup.wolf.framework.rpc.Result;
 import study.daydayup.wolf.framework.rpc.RpcService;
 import study.daydayup.wolf.framework.rpc.page.Page;
@@ -18,6 +20,26 @@ import study.daydayup.wolf.framework.rpc.page.PageRequest;
 public class TransactionServiceImpl implements TransactionService {
     @Override
     public Result<Page<Transaction>> query(TransactionQuery query, PageRequest pageRequest) {
+        if (StringUtil.notBlank(query.getPaymentNo())) {
+            return byPaymentNo(query.getPaymentNo(), query.getPayeeId());
+        }
+
+        if (StringUtil.notBlank(query.getSettlementNo())) {
+            return bySettlementNo(query, pageRequest);
+        }
+
+        return byType(query, pageRequest);
+    }
+
+    public Result<Page<Transaction>> byPaymentNo(@NonNull String paymentNo, @NonNull Long payeeId) {
+        return null;
+    }
+
+    public Result<Page<Transaction>> bySettlementNo(TransactionQuery query, PageRequest pageRequest) {
+        return null;
+    }
+
+    public Result<Page<Transaction>> byType(TransactionQuery query, PageRequest pageRequest) {
         return null;
     }
 }
