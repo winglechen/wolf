e849a20b724ee00e5a22d90aaea0cc33f9fca7f5
思源 <winglechen@gmail.com>
2020-06-22 12:04:38 +0800

    add SettlementController

--- app/estar/union-admin/src/main/java/study/daydayup/wolf/business/union/admin/controller/pay/SettlementController.java
+++ app/estar/union-admin/src/main/java/study/daydayup/wolf/business/union/admin/controller/pay/SettlementController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 import study.daydayup.wolf.business.account.auth.agent.Session;
-import study.daydayup.wolf.business.pay.api.domain.entity.Transaction;
-import study.daydayup.wolf.business.pay.api.dto.base.manage.TransactionQuery;
-import study.daydayup.wolf.business.pay.api.service.TransactionService;
+import study.daydayup.wolf.business.pay.api.domain.entity.Settlement;
+import study.daydayup.wolf.business.pay.api.dto.base.manage.SettlementQuery;
+import study.daydayup.wolf.business.pay.api.service.SettlementService;
 import study.daydayup.wolf.framework.layer.web.Controller;
 import study.daydayup.wolf.framework.rpc.Result;
 import study.daydayup.wolf.framework.rpc.page.Page;
@@ -23,21 +23,21 @@ import javax.annotation.Resource;
  * @since 2020/6/22 11:40 上午
  **/
 @RestController
-public class TransactionController implements Controller {
+public class SettlementController implements Controller {
     @Reference
-    private TransactionService transactionService;
+    private SettlementService settlementService;
     @Resource
     private Session session;
 
     @GetMapping("/pay/payment/query")
-    public Result<Page<Transaction>> find(@Validated @ModelAttribute TransactionQuery query) {
+    public Result<Page<Settlement>> find(@Validated @ModelAttribute SettlementQuery query) {
         Long orgId = session.get("orgId", Long.class);
-        query.setPayeeId(orgId);
+        query.setAccountId(orgId);
         PageRequest pageRequest = PageRequest.builder()
                 .pageNum(null == query.getPageNum() ? 1 : query.getPageNum())
                 .pageSize(10)
                 .build();
 
-        return transactionService.query(query, pageRequest);
+        return settlementService.query(query, pageRequest);
     }
 }
