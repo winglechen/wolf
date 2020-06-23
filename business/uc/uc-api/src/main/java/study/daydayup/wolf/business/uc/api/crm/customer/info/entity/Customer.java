d020ba5775c7de2be11957a35fe53c47583303d1
思源 <winglechen@gmail.com>
2020-06-23 16:38:14 +0800

    add customer related sql files

--- business/uc/uc-api/src/main/java/study/daydayup/wolf/business/uc/api/crm/customer/info/entity/Customer.java
+++ business/uc/uc-api/src/main/java/study/daydayup/wolf/business/uc/api/crm/customer/info/entity/Customer.java
@@ -1,47 +1,71 @@
 package study.daydayup.wolf.business.uc.api.crm.customer.info.entity;
 
 import lombok.Data;
-
 import study.daydayup.wolf.framework.layer.api.Model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 public class Customer implements Model {
+    private Long id;
 
     private Long accountId;
+
     private Long orgId;
 
-    private Long channelId;
-
-    private String nickname;
-    private String avatar;
+    private Long departmentId;
 
     private String realName;
-    private String mobile;
-    private String aadhaarNo;
-    private String panNo;
 
-    private String gender;
-    private String dob;
-    private Long creditAmount;
+    private String nickName;
+
+    private String avatar;
+
+    private LocalDate birthday;
+
+    private Boolean gender;
+
+    private String mobile;
+
+    private String tel;
+
+    private String email;
+
+    private String nationalId;
+
+    private Integer country;
+
+    private Integer province;
+
+    private Integer city;
+
+    private Integer district;
+
+    private String address;
+
+    private BigDecimal latitude;
+
+    private BigDecimal longitude;
 
     private String tags;
 
-    private Integer basicInfoAuthStatus;
-    private Integer livenessAuthStatus;
-    private Integer aadhaarAuthStatus;
-    private Integer panAuthStatus;
-    private Integer passportAuthStatus;
-    private Integer dlAuthStatus;
-    private Integer voterCardAuthStatus;
-    private Integer bankCardBindStatus;
-    private Integer kycStatus;
+    private String intro;
+
+    private String source;
+
+    private String deviceId;
 
     private Integer version;
+
     private Boolean deleteFlag;
+
     private Long lastEditor;
 
     private LocalDateTime createdAt;
+
     private LocalDateTime updatedAt;
 
     private static final long serialVersionUID = 1L;
