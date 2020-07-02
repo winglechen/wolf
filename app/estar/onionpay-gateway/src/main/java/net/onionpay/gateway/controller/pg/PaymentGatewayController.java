0f120dd2193c6237a4567a3cce1f6ec4d7ddf52e
思源 <winglechen@gmail.com>
2020-07-02 16:07:09 +0800

    add checkout method for PaymentGatewayController

--- app/estar/onionpay-gateway/src/main/java/net/onionpay/gateway/controller/pg/PaymentGatewayController.java
+++ app/estar/onionpay-gateway/src/main/java/net/onionpay/gateway/controller/pg/PaymentGatewayController.java
@@ -1,7 +1,10 @@
 package net.onionpay.gateway.controller.pg;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import study.daydayup.wolf.business.pay.api.domain.entity.Payment;
+import study.daydayup.wolf.business.trade.api.dto.buy.base.response.PayResponse;
 import study.daydayup.wolf.framework.rpc.Result;
 
 /**
@@ -10,6 +13,7 @@ import study.daydayup.wolf.framework.rpc.Result;
  * @author Wingle
  * @since 2020/7/2 3:51 下午
  **/
+@RestController
 public class PaymentGatewayController {
 
     @GetMapping("/pg/payment/detailByToken/{token}")
@@ -17,6 +21,9 @@ public class PaymentGatewayController {
         return null;
     }
 
-
+    @PostMapping("/pg/payment/checkout")
+    public Result<PayResponse> checkout() {
+        return null;
+    }
 
 }
