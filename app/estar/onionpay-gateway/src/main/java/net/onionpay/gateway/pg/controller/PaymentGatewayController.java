117f69d3666b45919e7f7e18c7ce3d7cc1de5f2d
思源 <winglechen@gmail.com>
2020-07-03 12:14:28 +0800

    refactor pg controller

--- app/estar/onionpay-gateway/src/main/java/net/onionpay/gateway/pg/controller/PaymentGatewayController.java
+++ app/estar/onionpay-gateway/src/main/java/net/onionpay/gateway/pg/controller/PaymentGatewayController.java
@@ -1,14 +1,17 @@
-package net.onionpay.gateway.controller.pg;
+package net.onionpay.gateway.pg.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import study.daydayup.wolf.business.pay.api.domain.entity.Payment;
 import study.daydayup.wolf.business.pay.api.dto.base.pay.PaymentCreateResponse;
 import study.daydayup.wolf.framework.rpc.Result;
 
+import net.onionpay.gateway.pg.dto.CheckoutRequest;
+
 /**
- * net.onionpay.gateway.controller.pg
+ * net.onionpay.gateway.pg
  *
  * @author Wingle
  * @since 2020/7/2 3:51 下午
@@ -22,7 +25,7 @@ public class PaymentGatewayController {
     }
 
     @PostMapping("/pg/payment/checkout")
-    public Result<PaymentCreateResponse> checkout() {
+    public Result<PaymentCreateResponse> checkout(@RequestBody CheckoutRequest request) {
         return null;
     }
 
---
+++ app/estar/onionpay-gateway/src/main/java/net/onionpay/gateway/pg/dto/CheckoutRequest.java
@@ -0,0 +1,16 @@
+package net.onionpay.gateway.pg.dto;
+
+import lombok.Data;
+import study.daydayup.wolf.framework.layer.api.DTO;
+
+/**
+ * net.onionpay.gateway.pg.dto
+ *
+ * @author Wingle
+ * @since 2020/7/3 12:05 下午
+ **/
+@Data
+public class CheckoutRequest implements DTO {
+    private String token;
+    private String paymentMode;
+}
