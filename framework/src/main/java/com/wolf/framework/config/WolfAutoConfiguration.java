679ba49015405d880231089b2365c05006efb597
noai <tech.yiguo@gmail.com>
2024-05-14 08:35:28 +0800

    rewrite nacos project.name to spring.application.name

--- framework/src/main/java/com/wolf/framework/config/WolfAutoConfiguration.java
+++ framework/src/main/java/com/wolf/framework/config/WolfAutoConfiguration.java
@@ -1,5 +1,7 @@
 package com.wolf.framework.config;
 
+import com.wolf.common.util.lang.StringUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
@@ -9,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import com.wolf.framework.middleware.rdb.DbExecutor;
 import com.wolf.framework.middleware.rdb.RDBTemplate;
+
+import javax.annotation.PostConstruct;
 
 /**
  * com.wolf.framework.config
@@ -20,6 +24,39 @@ import com.wolf.framework.middleware.rdb.RDBTemplate;
 @ComponentScan("com.wolf.framework")
 @AutoConfigureAfter(JdbcTemplateAutoConfiguration.class)
 public class WolfAutoConfiguration {
+
+    @Value("${spring.application.name}")
+    private String springApplicationName;
+
+    @PostConstruct
+    public void rewriteProperty() {
+        rewriteNacosProjectName();
+    }
+
+    /**
+     * set nacos project.name some to spring.application.name
+     *
+     * @see com.alibaba.nacos.client.utils.AppNameUtils
+     */
+    private void rewriteNacosProjectName() {
+        if (StringUtil.isBlank(springApplicationName)) {
+            // let it go, do nothing.
+            return;
+        }
+
+        String projectName = System.getProperty("project.name");
+
+        if (StringUtil.isNoneBlank(projectName)) {
+            // let it go, nothing to do.
+            // -D has a higher priority
+            return;
+        }
+
+        if (null == projectName || StringUtil.isBlank(projectName)) {
+            //set project.name some to spring.application.name
+            System.setProperty("project.name", springApplicationName);
+        }
+    }
 
     @Bean
     @ConditionalOnBean(JdbcTemplate.class)
