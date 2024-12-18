e12069fac2da6209c7c756304170eda6fa81ea27
winglechen@aliyun.com <winglechen@gmail.com>
2024-12-18 14:07:47 +0800

    feature: remove junit scope of common

--- common/src/main/java/com/wolf/common/convention/container/Context.java
+++ common/src/main/java/com/wolf/common/convention/container/Context.java
@@ -1,10 +1,12 @@
 package com.wolf.common.convention.container;
 
+import java.io.Serializable;
+
 /**
  * com.wolf.framework.layer.context
  *
  * @author Wingle
  * @since 2020/1/9 9:08 下午
  **/
-public interface Context {
+public interface Context extends Serializable {
 }
