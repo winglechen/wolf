c0de654e73b082620feae62cf08df9d5b50c30af
winglechen@aliyun.com <winglechen@gmail.com>
2024-12-20 12:45:12 +0800

    feature: add LifecycleManager

--- common/src/main/java/com/wolf/common/convention/service/Lifecycle.java
+++ common/src/main/java/com/wolf/common/convention/service/Lifecycle.java
@@ -2,8 +2,31 @@ package com.wolf.common.convention.service;
 
 public interface Lifecycle {
     void start();
-
     void shutdown();
 
-    boolean isRunning();
+    /**
+     * Initialization
+     */
+    void initialize();
+
+    /**
+     * cleanup
+     */
+    void cleanup();
+
+    State getState();
+
+    default boolean isRunning() {
+        return State.RUNNING.equals(getState());
+    }
+
+    enum State {
+        INITIALIZING,
+        STARTING,
+        RUNNING,
+        STOPPING,
+        STOPPED,
+        SHUTTING_DOWN,
+        TERMINATED
+    }
 }
