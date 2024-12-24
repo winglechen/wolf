ea4da0d62d36e92c0439e3f324255634803397d3
winglechen@aliyun.com <winglechen@gmail.com>
2024-12-24 16:14:33 +0800

    feature: add BufferUtil

---
+++ common/src/main/java/com/wolf/common/util/io/BufferUtil.java
@@ -0,0 +1,14 @@
+package com.wolf.common.util.io;
+
+import io.netty.util.internal.PlatformDependent;
+import java.nio.ByteBuffer;
+
+public class BufferUtil {
+    public static void cleanBuffer(ByteBuffer buffer) {
+        if (null == buffer || !buffer.isDirect() || buffer.capacity() == 0) {
+            return;
+        }
+
+        PlatformDependent.freeDirectBuffer(buffer);
+    }
+}
