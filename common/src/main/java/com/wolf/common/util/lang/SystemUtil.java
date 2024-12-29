49801e331b6e83a84cb9eef82f4bbb33b428c000
winglechen@aliyun.com <winglechen@gmail.com>
2024-12-29 16:21:30 +0800

    feature: add empty method getPageSize to SystemUtil

--- common/src/main/java/com/wolf/common/util/lang/SystemUtil.java
+++ common/src/main/java/com/wolf/common/util/lang/SystemUtil.java
@@ -1,7 +1,12 @@
 package com.wolf.common.util.lang;
 
+
 public class SystemUtil {
     public static long getPID() {
         return ProcessHandle.current().pid();
+    }
+
+    public static long getPageSize() {
+        return 0;
     }
 }
