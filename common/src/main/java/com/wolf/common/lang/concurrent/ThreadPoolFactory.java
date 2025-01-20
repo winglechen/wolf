8e314adc892aa47e68e4a563d27f5635e9137afc
winglechen@aliyun.com <winglechen@gmail.com>
2025-01-20 12:42:27 +0800

    feature: add some methods to ThreadUtil

---
+++ common/src/main/java/com/wolf/common/lang/concurrent/ThreadPoolFactory.java
@@ -0,0 +1,7 @@
+package com.wolf.common.lang.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadPoolFactory {
+}
--- common/src/main/java/com/wolf/common/util/lang/ThreadUtil.java
+++ common/src/main/java/com/wolf/common/util/lang/ThreadUtil.java
@@ -1,7 +1,18 @@
 package com.wolf.common.util.lang;
 
+import com.wolf.common.lang.concurrent.DefaultThreadFactory;
+import com.wolf.common.lang.concurrent.FutureThreadPoolExecutor;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
-import com.wolf.common.lang.exception.lang.InterruptedException;
 
 /**
  * com.wolf.common.util.lang
@@ -11,6 +22,178 @@ import com.wolf.common.lang.exception.lang.InterruptedException;
  **/
 @Slf4j
 public class ThreadUtil {
+    public static Thread create(String name, Runnable runnable, boolean daemon) {
+        Thread thread = new Thread(runnable, name);
+        thread.setDaemon(daemon);
+        thread.setUncaughtExceptionHandler(
+            (t, e) -> log.error("Uncaught exception in thread '{}':", t.getName(), e)
+        );
+        return thread;
+    }
+
+    /**
+     * Shutdown passed thread using isAlive and join.
+     *
+     * @param t Thread to stop
+     */
+    public static void shutdown(final Thread t) {
+        shutdown(t, 0);
+    }
+
+    /**
+     * Shutdown passed thread using isAlive and join.
+     *
+     * @param millis Pass 0 if we're to wait forever.
+     * @param t      Thread to stop
+     */
+    public static void shutdown(final Thread t, final long millis) {
+        if (t == null) {
+            return;
+        }
+        while (t.isAlive()) {
+            try {
+                t.interrupt();
+                t.join(millis);
+            } catch (java.lang.InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    /**
+     * An implementation of the graceful stop sequence recommended by
+     * {@link ExecutorService}.
+     *
+     * @param executor executor
+     * @param timeout  timeout
+     * @param timeUnit timeUnit
+     */
+    public static void shutdown(ExecutorService executor, long timeout, TimeUnit timeUnit) {
+        // Disable new tasks from being submitted.
+        executor.shutdown();
+        try {
+            // Wait a while for existing tasks to terminate.
+            if (!executor.awaitTermination(timeout, timeUnit)) {
+                executor.shutdownNow();
+                // Wait a while for tasks to respond to being cancelled.
+                if (!executor.awaitTermination(timeout, timeUnit)) {
+                    log.warn(String.format("%s didn't terminate!", executor));
+                }
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted.
+            executor.shutdownNow();
+            // Preserve interrupt status.
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static ExecutorService newSingleThreadExecutor(String processName, boolean isDaemon) {
+        return newSingleThreadExecutor(newThreadFactory(processName, isDaemon));
+    }
+
+    public static ExecutorService newSingleThreadExecutor(ThreadFactory threadFactory) {
+        return newThreadPoolExecutor(1, threadFactory);
+    }
+
+    public static ExecutorService newThreadPoolExecutor(int corePoolSize, ThreadFactory threadFactory) {
+        return newThreadPoolExecutor(
+            corePoolSize,
+            corePoolSize,
+            0L,
+            TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<>(),
+            threadFactory
+        );
+    }
+
+    public static ExecutorService newThreadPoolExecutor(int corePoolSize,
+        int maximumPoolSize,
+        long keepAliveTime,
+        TimeUnit unit, BlockingQueue<Runnable> workQueue,
+        String processName,
+        boolean isDaemon) {
+        return newThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, newThreadFactory(processName, isDaemon));
+    }
+
+    public static ExecutorService newThreadPoolExecutor(final int corePoolSize,
+        final int maximumPoolSize,
+        final long keepAliveTime,
+        final TimeUnit unit,
+        final BlockingQueue<Runnable> workQueue,
+        final ThreadFactory threadFactory) {
+        return newThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, new ThreadPoolExecutor.AbortPolicy());
+    }
+
+    public static ExecutorService newThreadPoolExecutor(int corePoolSize,
+        int maximumPoolSize,
+        long keepAliveTime,
+        TimeUnit unit,
+        BlockingQueue<Runnable> workQueue,
+        ThreadFactory threadFactory,
+        RejectedExecutionHandler handler) {
+        return new FutureThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
+    }
+
+    public static ScheduledExecutorService newSingleScheduledThreadExecutor(String processName, boolean isDaemon) {
+        return newScheduledThreadPool(1, processName, isDaemon);
+    }
+
+    public static ScheduledExecutorService newSingleScheduledThreadExecutor(ThreadFactory threadFactory) {
+        return newScheduledThreadPool(1, threadFactory);
+    }
+
+    public static ScheduledExecutorService newScheduledThreadPool(int corePoolSize) {
+        return newScheduledThreadPool(corePoolSize, Executors.defaultThreadFactory());
+    }
+
+    public static ScheduledExecutorService newScheduledThreadPool(int corePoolSize, String processName,
+        boolean isDaemon) {
+        return newScheduledThreadPool(corePoolSize, newThreadFactory(processName, isDaemon));
+    }
+
+    public static ScheduledExecutorService newScheduledThreadPool(int corePoolSize, ThreadFactory threadFactory) {
+        return newScheduledThreadPool(corePoolSize, threadFactory, new ThreadPoolExecutor.AbortPolicy());
+    }
+
+    public static ScheduledExecutorService newScheduledThreadPool(int corePoolSize,
+        ThreadFactory threadFactory,
+        RejectedExecutionHandler handler) {
+        return new ScheduledThreadPoolExecutor(corePoolSize, threadFactory, handler);
+    }
+
+    public static ThreadFactory newThreadFactory(String processName, boolean isDaemon) {
+        return newGenericThreadFactory("ThreadUtils-" + processName, isDaemon);
+    }
+
+    public static ThreadFactory newGenericThreadFactory(String processName) {
+        return newGenericThreadFactory(processName, false);
+    }
+
+    public static ThreadFactory newGenericThreadFactory(String processName, int threads) {
+        return newGenericThreadFactory(processName, threads, false);
+    }
+
+    public static ThreadFactory newGenericThreadFactory(final String processName, final boolean isDaemon) {
+        return new DefaultThreadFactory(processName + "_", isDaemon);
+    }
+
+    public static ThreadFactory newGenericThreadFactory(final String processName, final int threads,
+        final boolean isDaemon) {
+        return new DefaultThreadFactory(String.format("%s_%d_", processName, threads), isDaemon);
+    }
+
+    public static String geStackTrace() {
+        StringBuilder sb = new StringBuilder();
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        for (StackTraceElement ste : stackTrace) {
+            sb.append("\n\t");
+            sb.append(ste.toString());
+        }
+
+        return sb.toString();
+    }
+
     public static boolean sleep(long millis) {
         return ThreadUtil.sleep(millis, null, null);
     }
